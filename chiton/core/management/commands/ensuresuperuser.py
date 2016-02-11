d973ef6a96acb2840503a5ea033e7391b0b9614e
Justin Locsei <justin.locsei@gmail.com>
2016-02-10 20:44:54 -0500

    Add a management command to ensure that a superuser exists

---
+++ chiton/core/management/commands/ensuresuperuser.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from chiton.utils.users import ensure_superuser_exists
+
+
+class Command(BaseCommand):
+    help = "Ensure that a superuser exists"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--email", type=str, required=True)
+        parser.add_argument("--password", type=str, required=True)
+        parser.add_argument("--username", type=str, required=True)
+
+    def handle(self, *arg, **options):
+        email = options['email']
+        password = options['password']
+        username = options['username']
+
+        ensure_superuser_exists(username, email, password)
