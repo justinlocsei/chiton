1cde0c7d67e1b9b72c8bca12c0a43074c97d0d22
Justin Locsei <justin.locsei@gmail.com>
2016-02-10 20:14:57 -0500

    Use the network utility for the devserver command

--- chiton/core/management/commands/devserver.py
+++ chiton/core/management/commands/devserver.py
@@ -3,15 +3,15 @@ import os
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
+from chiton.utils.network import resolve_binding
+
 
 class Command(BaseCommand):
     help = "Run a development server on a and address port set by environment variables"
 
     def handle(self, *arg, **options):
-        address = os.environ.get("CHITON_SERVER_ADDRESS")
-        port = os.environ.get("CHITON_SERVER_PORT")
+        address = os.environ.get("CHITON_SERVER_ADDRESS", "")
+        port = os.environ.get("CHITON_SERVER_PORT", "")
 
-        bindings = [address, port]
-        binding = ":".join([str(b) for b in bindings if b])
-
+        binding = resolve_binding(address, port=port)
         call_command("runserver", binding)
