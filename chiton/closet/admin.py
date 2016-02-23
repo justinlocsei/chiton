5941a833641add870ab74f2f8d5c7344ae1d8c48
Justin Locsei <justin.locsei@gmail.com>
2016-02-22 19:59:08 -0500

    Add basic admin options for the closet admin

--- chiton/closet/admin.py
+++ chiton/closet/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 from chiton.closet import models
 from chiton.core.admin import site
@@ -6,21 +7,30 @@ from chiton.core.admin import site
 
 @admin.register(models.Garment, site=site)
 class GarmentAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ('name', 'line', 'category')
+    list_filter = ('line', 'category')
+    ordering = ('name',)
+    search_fields = ['name']
 
 
 @admin.register(models.Brand, site=site)
 class BrandAdmin(admin.ModelAdmin):
-    pass
+
+    ordering = ('name',)
+    search_fields = ['name']
 
 
 @admin.register(models.Line, site=site)
 class LineAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ('name', 'brand')
+    list_filter = ('brand',)
+    ordering = ('name', 'brand__name')
 
 
 @admin.register(models.GarmentCategory, site=site)
-class GarmentCategoryAdmin(admin.ModelAdmin):
+class GarmentCategoryAdmin(MPTTModelAdmin):
     pass
 
 
