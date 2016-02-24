f3099c70b75f1d6200bb2061d7d74fddaed4a3dd
Justin Locsei <justin.locsei@gmail.com>
2016-02-23 20:41:48 -0500

    Add an exception for known filesystem errors

--- chiton/core/exceptions.py
+++ chiton/core/exceptions.py
@@ -1,2 +1,6 @@
 class ConfigurationError(Exception):
     """An error indicating improper configuration."""
+
+
+class FilesystemError(Exception):
+    """An error indicating an issue with the filesystem."""
