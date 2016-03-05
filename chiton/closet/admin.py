3d8fd3e2cff8b802e3d87082ac65e22c2fb4d97d
Justin Locsei <justin.locsei@gmail.com>
2016-03-04 20:53:17 -0500

    Alphabetize the closet admin file

--- chiton/closet/admin.py
+++ chiton/closet/admin.py
@@ -4,6 +4,21 @@ from chiton.closet import models
 from chiton.core.admin import site
 
 
+@admin.register(models.Brand, site=site)
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'age_lower', 'age_upper')
+    ordering = ('name',)
+    search_fields = ['name']
+
+
+@admin.register(models.Formality, site=site)
+class FormalityAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+    ordering = ('name',)
+
+
 @admin.register(models.Garment, site=site)
 class GarmentAdmin(admin.ModelAdmin):
 
@@ -13,23 +28,8 @@ class GarmentAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
-@admin.register(models.Brand, site=site)
-class BrandAdmin(admin.ModelAdmin):
-
-    list_display = ('name', 'age_lower', 'age_upper')
-    ordering = ('name',)
-    search_fields = ['name']
-
-
 @admin.register(models.Style, site=site)
 class StyleAdmin(admin.ModelAdmin):
-
-    list_display = ('name',)
-    ordering = ('name',)
-
-
-@admin.register(models.Formality, site=site)
-class FormalityAdmin(admin.ModelAdmin):
 
     list_display = ('name',)
     ordering = ('name',)
