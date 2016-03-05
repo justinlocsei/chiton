82670264536e8cad85025162d2809246312b3648
Justin Locsei <justin.locsei@gmail.com>
2016-03-04 20:53:56 -0500

    Alphabetize the closet tests

--- tests/unit/closet/test_models.py
+++ tests/unit/closet/test_models.py
@@ -4,19 +4,6 @@ from django.test import TestCase
 from chiton.closet.models import Brand, Formality, Garment, Style
 
 
-class GarmentTestCase(TestCase):
-
-    def test_natural_key(self):
-        """It uses its slug and the slug of its brand."""
-        brand = Brand.objects.create(name="Givenchy")
-
-        garment = Garment.objects.create(name="Cocktail Dress", brand=brand)
-        self.assertEqual(garment.natural_key(), ('cocktail-dress', 'givenchy'))
-
-        found = Garment.objects.get_by_natural_key('cocktail-dress', 'givenchy')
-        self.assertEqual(garment.pk, found.pk)
-
-
 class BrandTestCase(TestCase):
 
     def test_natural_key(self):
@@ -40,17 +27,6 @@ class BrandTestCase(TestCase):
         self.assertIsNone(brand.full_clean())
 
 
-class StyleTestCase(TestCase):
-
-    def test_natural_key(self):
-        """It uses its slug."""
-        style = Style.objects.create(slug="nervous-skittish")
-        self.assertEqual(style.natural_key(), ('nervous-skittish',))
-
-        found = Style.objects.get_by_natural_key('nervous-skittish')
-        self.assertEqual(style.pk, found.pk)
-
-
 class FormalityTestCase(TestCase):
 
     def test_natural_key(self):
@@ -60,3 +36,27 @@ class FormalityTestCase(TestCase):
 
         found = Formality.objects.get_by_natural_key('snappy-boardroom')
         self.assertEqual(formality.pk, found.pk)
+
+
+class GarmentTestCase(TestCase):
+
+    def test_natural_key(self):
+        """It uses its slug and the slug of its brand."""
+        brand = Brand.objects.create(name="Givenchy")
+
+        garment = Garment.objects.create(name="Cocktail Dress", brand=brand)
+        self.assertEqual(garment.natural_key(), ('cocktail-dress', 'givenchy'))
+
+        found = Garment.objects.get_by_natural_key('cocktail-dress', 'givenchy')
+        self.assertEqual(garment.pk, found.pk)
+
+
+class StyleTestCase(TestCase):
+
+    def test_natural_key(self):
+        """It uses its slug."""
+        style = Style.objects.create(slug="nervous-skittish")
+        self.assertEqual(style.natural_key(), ('nervous-skittish',))
+
+        found = Style.objects.get_by_natural_key('nervous-skittish')
+        self.assertEqual(style.pk, found.pk)
