6287c9e45ab1663a18e37e0e96af7923543282b9
Justin Locsei <justin.locsei@gmail.com>
2016-03-10 21:58:49 -0500

    Add a form for entering affiliate items via network and item URL

---
+++ chiton/rack/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from chiton.rack.models import AffiliateItem
+from chiton.rack.affiliates import create_affiliate
+
+api_field_options = {
+    'help_text': _('This is automatically populated by the API'),
+    'widget': forms.TextInput(attrs={'size': 100, 'readonly': True})
+}
+
+
+class AffiliateItemURLForm(forms.ModelForm):
+    """A form that fetches additional data on an affiliate item from its URL.
+
+    This requires only the network and URL, and uses those values to request
+    more data on the item via the network's API.
+    """
+
+    guid = forms.CharField(required=False, label=_('GUID'), **api_field_options)
+    name = forms.CharField(required=False, label=_('Name'), **api_field_options)
+    url = forms.CharField(required=True, label=_('URL'), widget=forms.URLInput(attrs={'size': 100}))
+
+    class Meta:
+        model = AffiliateItem
+        fields = ['network', 'url', 'guid', 'name']
+
+    def clean(self):
+        """Get overview data from the affiliate if a URL and network are set."""
+        super().clean()
+
+        network = self.cleaned_data.get('network')
+        url = self.cleaned_data.get('url')
+
+        if network and url:
+            affiliate = create_affiliate(network.slug)
+            response = affiliate.request_overview(url)
+            self.cleaned_data['guid'] = response['guid']
+            self.cleaned_data['name'] = response['name']
