769ddfc1289e2812c569a021fc93d784a3c54f6d
Justin Locsei <justin.locsei@gmail.com>
2016-03-10 21:59:21 -0500

    Integrate the affiliate URL form with affiliate items in the admin

--- chiton/rack/admin.py
+++ chiton/rack/admin.py
@@ -3,12 +3,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from chiton.core.admin import site
 from chiton.rack import models
+from chiton.rack.forms import AffiliateItemURLForm
 
 
 @admin.register(models.AffiliateItem, site=site)
 class AffiliateItemAdmin(admin.ModelAdmin):
 
-    list_display = ('network_name', 'created_at')
+    form = AffiliateItemURLForm
+    list_display = ('name', 'guid', 'network_name', 'created_at')
+    list_filter = ('network',)
     ordering = ('created_at',)
 
     def network_name(self, obj):
