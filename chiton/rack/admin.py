d04a2af824ce347e554a6eb80935400a3d47f1fa
Justin Locsei <justin.locsei@gmail.com>
2016-03-11 20:30:20 -0500

    Add a view to show API responses for an affiliate item

--- chiton/rack/admin.py
+++ chiton/rack/admin.py
@@ -1,8 +1,15 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
+import json
+import pygments
+from pygments.lexers import JsonLexer
+from pygments.formatters import HtmlFormatter
 
 from chiton.core.admin import site
 from chiton.rack import models
+from chiton.rack.affiliates import create_affiliate
 from chiton.rack.forms import AffiliateItemURLForm
 
 
@@ -18,6 +25,31 @@ class AffiliateItemAdmin(admin.ModelAdmin):
         return obj.network.name
     network_name.admin_order_field = 'network__name'
     network_name.short_description = _('Affiliate Network')
+
+    def get_urls(self):
+        core = super().get_urls()
+        custom = [
+            url(r'^(?P<pk>\d+)/api/$', self.admin_site.admin_view(self.api_details), name='affiliate-item-api-details')
+        ]
+        return custom + core
+
+    def api_details(self, request, pk):
+        item = models.AffiliateItem.objects.get(pk=pk)
+
+        affiliate = create_affiliate(slug=item.network.slug)
+        overview = affiliate.request_overview(item.url)
+
+        api_response = json.dumps(overview, sort_keys=True, indent=4)
+        api_json = pygments.highlight(api_response, JsonLexer(), HtmlFormatter())
+        api_styles = HtmlFormatter().get_style_defs('.highlight')
+
+        return TemplateResponse(request, 'admin/chiton_rack/affiliateitem/api_details.html', dict(
+            self.admin_site.each_context(request),
+            api_response_json=api_json,
+            api_response_styles=api_styles,
+            item=item,
+            title=item.name
+        ))
 
 
 @admin.register(models.AffiliateNetwork, site=site)
