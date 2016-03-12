e94d462eba1310be86fb9398d63acd5ae228a4f4
Justin Locsei <justin.locsei@gmail.com>
2016-03-12 12:06:43 -0500

    Simplify the Amazon API matcher

--- tests/helpers/vcr.py
+++ tests/helpers/vcr.py
@@ -1,6 +1,5 @@
 from functools import partial
 import os.path
-import urllib.parse as urlparse
 
 from inflection import underscore
 import pytest
@@ -19,33 +18,10 @@ def request_to_cassette_path(request):
     return os.path.join(*paths)
 
 
-def match_amazon_api_request(r1, r2):
-    """Ignore the timestamp and signature in Amazon API requests."""
-    p1 = pin_amazon_api_request_url(r1.uri)
-    p2 = pin_amazon_api_request_url(r2.uri)
-    return p1 == p2
-
-
-def pin_amazon_api_request_url(url):
-    """Return an Amazon API request URL without transient information."""
-    parsed = urlparse.urlparse(url)
-
-    query = urlparse.parse_qs(parsed.query)
-    query.pop('Signature')
-    query.pop('Timestamp')
-
-    rebuilt = list(parsed)
-    rebuilt[4] = urlparse.urlencode(query)
-
-    return urlparse.urlunparse(rebuilt)
-
-
 @pytest.fixture
 def amazon_api_request(request):
     vcr = VCR()
-    vcr.register_matcher('amazon_api', match_amazon_api_request)
-    vcr.match_on = ['amazon_api']
 
-    cassette_path = request_to_cassette_path(request)
-
-    return partial(vcr.use_cassette, cassette_path)
+    return partial(vcr.use_cassette,
+        request_to_cassette_path(request),
+        filter_query_parameters=['Signature', 'Timestamp'])
