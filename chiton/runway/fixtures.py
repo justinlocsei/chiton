904b909310df89219e91431214a6f8fd3c10e38d
Justin Locsei <justin.locsei@gmail.com>
2016-03-13 21:16:34 -0400

    Add a fixture for categories

--- chiton/runway/fixtures.py
+++ chiton/runway/fixtures.py
@@ -1,10 +1,11 @@
 from chiton.runway.apps import Config as App
-from chiton.runway.models import Formality, Style
+from chiton.runway.models import Category, Formality, Style
 from chiton.core.data import Fixture
 
 
 def load_fixtures():
     return [
+        Fixture(App, 'categories', Category.objects.all()),
         Fixture(App, 'formalities', Formality.objects.all()),
         Fixture(App, 'styles', Style.objects.all())
     ]
