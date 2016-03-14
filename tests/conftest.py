f9f088d4c18d0931e1d8ebc5be243c2d105f8e2d
Justin Locsei <justin.locsei@gmail.com>
2016-03-14 09:06:44 -0400

    Add factories for runway models

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
+from pytest_factoryboy import register as register_factory
 
+from .helpers.factories.runway import BasicFactory, CategoryFactory
 from .helpers.vcr import amazon_api_request, shopstyle_api_request
 
 pytest.fixture()(amazon_api_request)
 pytest.fixture()(shopstyle_api_request)
+
+register_factory(BasicFactory)
+register_factory(CategoryFactory)
---
+++ tests/helpers/factories/runway.py
@@ -0,0 +1,24 @@
+import factory
+from factory.django import DjangoModelFactory
+from faker import Faker
+
+from chiton.runway.models import Basic, Category
+
+fake = Faker()
+
+
+class CategoryFactory(DjangoModelFactory):
+
+    name = factory.Sequence(lambda n: '%s-%d' % (fake.sentence(), n))
+
+    class Meta:
+        model = Category
+
+
+class BasicFactory(DjangoModelFactory):
+
+    name = factory.Sequence(lambda n: '%s-%d' % (fake.sentence(), n))
+    category = factory.SubFactory(CategoryFactory)
+
+    class Meta:
+        model = Basic
