c7b88e1482b77e0aee216bdf9b3173a7867d3362
Justin Locsei <justin.locsei@gmail.com>
2016-03-15 20:30:40 -0400

    Load all core fixtures at once

--- chiton/core/management/commands/chiton_loadfixtures.py
+++ chiton/core/management/commands/chiton_loadfixtures.py
@@ -10,10 +10,18 @@ class Command(BaseCommand):
     help = 'Load all application fixtures'
 
     def handle(self, *arg, **options):
+        resolved = []
+
         for fixture in load_fixtures():
             file = fixture.find()
             if not os.path.isfile(file):
                 raise CommandError('The %s fixture could not be located' % fixture.label)
+            resolved.append((file, fixture.label))
 
-            self.stdout.write('Loading %s fixture...' % fixture.label)
-            call_command('loaddata', file)
+        labels = sorted([f[1] for f in resolved])
+        self.stdout.write('Loading fixtures:')
+        for label in labels:
+            self.stdout.write('  %s' % label)
+
+        files = [f[0] for f in resolved]
+        call_command('loaddata', *files)
