4496781a1ab0887dd1f825271fb9735114cce529
Justin Locsei <justin.locsei@gmail.com>
2016-04-15 09:44:47 -0400

    Make the title of the admin site more useful

--- chiton/core/admin.py
+++ chiton/core/admin.py
@@ -1,12 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 
 class AdminSite(admin.AdminSite):
 
-    site_header = _('Chiton')
-    site_title = _('Chiton')
-    index_title = _('Chiton administration')
+    site_header = _('Cover Your Basics (%(environment)s)') % {'environment': settings.ENVIRONMENT_NAME.capitalize()}
+    site_title = _('Cover Your Basics')
 
 
 site = AdminSite(name='chiton')
