e332a4dafab53820fc5db5bcbb870dfbb84bfb39
Justin Locsei <justin.locsei@gmail.com>
2016-04-16 13:58:00 -0400

    Aggressively cache the jsi18n URL

--- chiton/core/admin.py
+++ chiton/core/admin.py
@@ -1,12 +1,26 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.cache import cache_control
+from django.views.i18n import javascript_catalog
 
 
 class AdminSite(admin.AdminSite):
 
     site_header = _('Cover Your Basics (%(environment)s)') % {'environment': settings.ENVIRONMENT_NAME.capitalize()}
     site_title = _('Cover Your Basics')
+
+    def get_urls(self):
+        core = super().get_urls()
+        custom = [
+            url(r'^jsi18n/$', self.cached_jsi18n, name='jsi18n')
+        ]
+        return custom + core
+
+    @cache_control(private=True, max_age=60 * 60 * 24)
+    def cached_jsi18n(self, request):
+        return javascript_catalog(request, packages=['django.conf', 'django.contrib.admin'])
 
 
 site = AdminSite(name='chiton')
