8399dd6487d36449bb1348e73f8a43b6c9bc6c66
Justin Locsei <justin.locsei@gmail.com>
2016-04-26 20:15:25 -0400

    Add skeleton URLs for the API

--- chiton/urls.py
+++ chiton/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
+from chiton.api.urls import urlpatterns as api_urls
 from chiton.core import admin
 
 urlpatterns = [
-    url(r'^stockroom/', admin.site.urls),
+    url(r'^api/', include(api_urls)),
+    url(r'^stockroom/', admin.site.urls)
 ]
