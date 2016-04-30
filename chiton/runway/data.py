693a81d65642cc2d5330769e9be567f115883d3c
Justin Locsei <justin.locsei@gmail.com>
2016-04-30 13:01:16 -0400

    Explicitly mark basics that are not important as such

--- chiton/runway/data.py
+++ chiton/runway/data.py
@@ -2,6 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 PROPRIETY_IMPORTANCES = {
+    'NOT': 'not',
     'MILDLY': 'mildly',
     'SOMEWHAT': 'somewhat',
     'VERY': 'very',
@@ -9,6 +10,7 @@ PROPRIETY_IMPORTANCES = {
 }
 
 PROPRIETY_IMPORTANCE_CHOICES = (
+    (PROPRIETY_IMPORTANCES['NOT'], _('Not important')),
     (PROPRIETY_IMPORTANCES['MILDLY'], _('Not that important')),
     (PROPRIETY_IMPORTANCES['SOMEWHAT'], _('Somewhat important')),
     (PROPRIETY_IMPORTANCES['VERY'], _('Very important')),
