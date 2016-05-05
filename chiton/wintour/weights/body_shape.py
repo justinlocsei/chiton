8b338baa4bcbd5d113d911bd0d557304b0239643
Justin Locsei <justin.locsei@gmail.com>
2016-05-05 11:05:30 -0400

    Add debug logging to the body-shape weight

--- chiton/wintour/weights/body_shape.py
+++ chiton/wintour/weights/body_shape.py
@@ -1,5 +1,5 @@
-from chiton.closet.data import EMPHASES, PANT_RISES
-from chiton.wintour.data import BODY_SHAPES
+from chiton.closet.data import EMPHASES, EMPHASIS_CHOICES,  PANT_RISES
+from chiton.wintour.data import BODY_SHAPES, BODY_SHAPE_CHOICES
 from chiton.wintour.weights import BaseWeight
 
 
@@ -7,6 +7,12 @@ EMPHASIS_FIELDS = {
     'hip': 'hip_emphasis',
     'shoulder': 'shoulder_emphasis',
     'waist': 'waist_emphasis'
+}
+
+EMPHASIS_DELTA_STATES = {
+    0: 'flattering',
+    1: 'neutral',
+    2: 'unflattering'
 }
 
 EMPHASIS_RANKS = {
@@ -19,6 +25,12 @@ IMPORTANCES = {
     'LOW': 2,
     'MEDIUM': 3,
     'HIGH': 4
+}
+
+IMPORTANCE_DISPLAY = {
+    IMPORTANCES['LOW']: 'low',
+    IMPORTANCES['MEDIUM']: 'medium',
+    IMPORTANCES['HIGH']: 'high'
 }
 
 BODY_SHAPE_WEIGHTS = {
@@ -100,6 +112,23 @@ BODY_SHAPE_WEIGHTS = {
 }
 
 
+def _extract_choice(choices, id):
+    """Extract the display value of a choice from a list of choices.
+
+    Args:
+        choices (list): A list of two-tuple choices
+        id (str): The identifier for the choice
+
+    Returns:
+        str: The display name for the choice
+    """
+    for choice in choices:
+        if choice[0] == id:
+            return choice[1]
+
+    return None
+
+
 class BodyShapeWeight(BaseWeight):
     """A weight that compares a user's body shape with a garment's cut."""
 
@@ -108,10 +137,11 @@ class BodyShapeWeight(BaseWeight):
 
     def provide_profile_data(self, profile):
         return {
+            'body_shape': _extract_choice(BODY_SHAPE_CHOICES, profile.body_shape),
             'weights': BODY_SHAPE_WEIGHTS[profile.body_shape]
         }
 
-    def apply(self, garment, weights=None):
+    def apply(self, garment, body_shape=None, weights=None):
         weight = 0
 
         # Apply weights based on how distant the garment's body-part emphasis is
@@ -126,9 +156,27 @@ class BodyShapeWeight(BaseWeight):
             garment_weight = field_weight['importance'] / (actual_emphasis_delta + 1)
             weight += garment_weight
 
+            if self.debug:
+                reason = 'A %s %s emphasis is %s for a %s shape and of %s importance' % (
+                    _extract_choice(EMPHASIS_CHOICES, garment_emphasis).lower(),
+                    weight_name,
+                    EMPHASIS_DELTA_STATES[actual_emphasis_delta],
+                    body_shape.lower(),
+                    IMPORTANCE_DISPLAY[field_weight['importance']]
+                )
+                self.explain_weight(garment, garment_weight, reason)
+
         # Add bonus weights to the garment if it defines a pant rise that
         # matches one of the ideal rises for the wardrobe profile
+        pant_reason = None
+        pant_weight = IMPORTANCES['LOW']
         if garment.pant_rise and garment.pant_rise in weights['pant_rises']:
-            weight += IMPORTANCES['LOW']
+            weight += pant_weight
+            pant_reason = 'A %s pant rise is flattering for a %s shape' % (garment.pant_rise, body_shape.lower())
+        elif garment.pant_rise:
+            pant_reason = 'A %s pant rise is neutral for a %s shape' % (garment.pant_rise, body_shape.lower())
+
+        if self.debug and pant_reason:
+            self.explain_weight(garment, pant_weight, pant_reason)
 
         return weight
