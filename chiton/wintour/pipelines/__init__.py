2c92e78f3827f6966164de682ce2a12d362eb4fb
Justin Locsei <justin.locsei@gmail.com>
2016-05-05 15:19:12 -0400

    Split the pipeline workflow into smaller methods

--- chiton/wintour/pipelines/__init__.py
+++ chiton/wintour/pipelines/__init__.py
@@ -89,16 +89,56 @@ class BasePipeline:
             with filter_instance.apply_to_profile(profile) as filter_function:
                 garments_qs = filter_function(garments_qs)
 
-        # Apply each weight to each garment, and group the results by weight,
-        # while keeping data on the weight's value ranges and importance
+        # Generate the master list of weighted garments as a dict keyed by a
+        # basic instance with garment core data and metadata
+        weightings = self._weight_garments(weights, garments_qs, profile)
+        weighted_garments = self._combine_garment_weights(weightings, debug)
+        garments_by_basic = self._normalize_weightings(weighted_garments)
+
+        # Generate the final recommendations as a dict keyed by basic type, each
+        # of which will have a key for facets as well as a key for all garments,
+        # sorted in descending order by weight
+        recs = {}
+        weight_fetcher = itemgetter('weight')
+        for basic, weighted_garments in garments_by_basic.items():
+            sorted_garments = sorted(weighted_garments.values(), key=weight_fetcher, reverse=True)
+
+            faceted = {}
+            for facet in facets:
+                faceted[facet] = facet.apply(sorted_garments)
+
+            recs[basic] = {
+                'basic': basic,
+                'facets': faceted,
+                'garments': sorted_garments
+            }
+
+        # TODO: Delete once all steps are implemented
+        for query in connection.queries:
+            print(query['sql'])
+            print(query['time'])
+        print('Total queries: %d' % len(connection.queries))
+
+        return recs
+
+    def _weight_garments(self, weights, garments, profile):
+        """Apply a series of weights to a list of garments for a profile.
+
+        This applies each weight to each garment, and exposes the results in a
+        dict keyed by the weight instance with values of the weighted garments
+        and the weight's relative importance.
+        """
         weightings = {}
+
         for weight in weights:
             weighted_garments = []
             max_weight = 0
             min_weight = 0
 
+            # Apply the weight to the garment, and update the max and min
+            # weights in response to the results
             with weight.apply_to_profile(profile) as weight_function:
-                for garment in garments_qs:
+                for garment in garments:
                     applied_weight = weight_function(garment)
                     weighted_garments.append({
                         'garment': garment,
@@ -120,10 +160,18 @@ class BasePipeline:
                 'importance': weight.importance
             }
 
-        # Build a master list of weighted garments by combining the weight
-        # values applied to each garment and using each weight's importance to
-        # determine the final applied weight
+        return weightings
+
+    def _combine_garment_weights(self, weightings, debug):
+        """Combine all weights applied to a garment.
+
+        This expects to receive a dict keyed by weight instances with values of
+        the garments in the weight and the weight's importance.  This structure
+        is used to build a dict keyed by garment instance that exposes core
+        garment data and weighting metadata.
+        """
         weighted_garments = {}
+
         for weight, data in weightings.items():
             for weighted_garment in data['garments']:
                 garment = weighted_garment['garment']
@@ -158,6 +206,19 @@ class BasePipeline:
                         'action': weight_action
                     })
 
+        return weighted_garments
+
+    def _normalize_weightings(self, weighted_garments):
+        """Normalize weighted garments.
+
+        This expects a dict keyed by a garment instance with values that
+        describe the garment's weighting.  This information is converted into a
+        dict keyed by a basic instance that exposes normalized weighting data
+        and supplemental garment data.
+        """
+        by_basic = {}
+        max_weight = 0
+
         # Build a lookup table mapping affiliate-network PKs to network names
         affiliate_networks = {}
         for network in AffiliateNetwork.objects.all():
@@ -165,8 +226,6 @@ class BasePipeline:
 
         # Group garments by their basic type, exposing information on each
         # garment's associated affiliate items
-        max_weight = 0
-        by_basic = {}
         for affiliate_item in AffiliateItem.objects.all().select_related('garment__basic'):
             garment = affiliate_item.garment
             try:
@@ -198,28 +257,5 @@ class BasePipeline:
                 for garment, data in garments.items():
                     data['weight'] = data['weight'] / max_weight
 
-        # Generate the final recommendations as a dict keyed by basic type, each
-        # of which will have a key for facets as well as a key for all garments,
-        # sorted in descending order by weight
-        recs = {}
-        weight_fetcher = itemgetter('weight')
-        for basic, weighted_garments in by_basic.items():
-            sorted_garments = sorted(weighted_garments.values(), key=weight_fetcher, reverse=True)
+        return by_basic
 
-            faceted = {}
-            for facet in facets:
-                faceted[facet] = facet.apply(sorted_garments)
-
-            recs[basic] = {
-                'basic': basic,
-                'facets': faceted,
-                'garments': sorted_garments
-            }
-
-        # TODO: Delete once all steps are implemented
-        for query in connection.queries:
-            print(query['sql'])
-            print(query['time'])
-        print('Total queries: %d' % len(connection.queries))
-
-        return recs
