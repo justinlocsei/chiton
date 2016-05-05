9279c7df7c28a72738d04e0e41d195904397da27
Justin Locsei <justin.locsei@gmail.com>
2016-05-05 13:14:17 -0400

    Simplify the body-shape weight

--- chiton/wintour/weights/body_shape.py
+++ chiton/wintour/weights/body_shape.py
@@ -1,38 +1,46 @@
-from chiton.closet.data import EMPHASES, EMPHASIS_CHOICES, PANT_RISES
-from chiton.wintour.data import BODY_SHAPES, BODY_SHAPE_CHOICES
+from chiton.closet.data import EMPHASES, EMPHASIS_DISPLAY, PANT_RISES
+from chiton.wintour.data import BODY_SHAPES, BODY_SHAPE_DISPLAY
 from chiton.wintour.weights import BaseWeight
 
 
+# A mapping of internal emphasis keys to fields on the Garment model
 EMPHASIS_FIELDS = {
     'hip': 'hip_emphasis',
     'shoulder': 'shoulder_emphasis',
     'waist': 'waist_emphasis'
 }
 
+# A mapping of steps away from the ideal emphasis to a readable adjective
 EMPHASIS_DELTA_STATES = {
     0: 'flattering',
     1: 'neutral',
     2: 'unflattering'
 }
 
+# A mappnig of emphases to their numeric weight
 EMPHASIS_RANKS = {
     EMPHASES['WEAK']: 0,
     EMPHASES['NEUTRAL']: 1,
     EMPHASES['STRONG']: 2
 }
 
+# A mapping of internal importance IDs to their weight
 IMPORTANCES = {
     'LOW': 2,
     'MEDIUM': 3,
     'HIGH': 4
 }
 
+# A mapping of importances to readable values
 IMPORTANCE_DISPLAY = {
     IMPORTANCES['LOW']: 'low',
     IMPORTANCES['MEDIUM']: 'medium',
     IMPORTANCES['HIGH']: 'high'
 }
 
+# A map that defines the ideal garment style for each body shape.  This includes
+# the best emphasis for each body part, the importance of finding a good
+# emphasis, and information on ideal pant rises.
 BODY_SHAPE_WEIGHTS = {
     BODY_SHAPES['APPLE']: {
         'shoulder': {
@@ -112,23 +120,6 @@ BODY_SHAPE_WEIGHTS = {
 }
 
 
-def _extract_choice(choices, id):
-    """Extract the display value of a choice from a list of choices.
-
-    Args:
-        choices (list): A list of two-tuple choices
-        id (str): The identifier for the choice
-
-    Returns:
-        str: The display name for the choice
-    """
-    for choice in choices:
-        if choice[0] == id:
-            return choice[1]
-
-    return None
-
-
 class BodyShapeWeight(BaseWeight):
     """A weight that compares a user's body shape with a garment's cut."""
 
@@ -137,7 +128,7 @@ class BodyShapeWeight(BaseWeight):
 
     def provide_profile_data(self, profile):
         return {
-            'body_shape': _extract_choice(BODY_SHAPE_CHOICES, profile.body_shape),
+            'body_shape': BODY_SHAPE_DISPLAY[profile.body_shape],
             'weights': BODY_SHAPE_WEIGHTS[profile.body_shape]
         }
 
@@ -148,26 +139,26 @@ class BodyShapeWeight(BaseWeight):
         # from the ideal emphasis for a body shape and the importance of having
         # a garment that matches the ideal shape for the wardrobe profile
         for weight_name, field_name in EMPHASIS_FIELDS.items():
-            field_weight = weights[weight_name]
-            ideal_emphasis = field_weight['emphasis']
+            body_part_weight = weights[weight_name]
+            ideal_emphasis = body_part_weight['emphasis']
             garment_emphasis = getattr(garment, field_name)
             actual_emphasis_delta = abs(EMPHASIS_RANKS[ideal_emphasis] - EMPHASIS_RANKS[garment_emphasis])
 
-            garment_weight = field_weight['importance'] / (actual_emphasis_delta + 1)
+            garment_weight = body_part_weight['importance'] / (actual_emphasis_delta + 1)
             weight += garment_weight
 
             if self.debug:
                 reason = 'A %s %s emphasis is %s for a %s shape and of %s importance' % (
-                    _extract_choice(EMPHASIS_CHOICES, garment_emphasis).lower(),
+                    EMPHASIS_DISPLAY[garment_emphasis].lower(),
                     weight_name,
                     EMPHASIS_DELTA_STATES[actual_emphasis_delta],
                     body_shape.lower(),
-                    IMPORTANCE_DISPLAY[field_weight['importance']]
+                    IMPORTANCE_DISPLAY[body_part_weight['importance']]
                 )
                 self.explain_weight(garment, garment_weight, reason)
 
         # Add bonus weights to the garment if it defines a pant rise that
-        # matches one of the ideal rises for the wardrobe profile
+        # matches one of the ideal rises for the body shape
         pant_reason = None
         pant_weight = IMPORTANCES['LOW']
         if garment.pant_rise and garment.pant_rise in weights['pant_rises']:
