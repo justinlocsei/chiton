331ed0ba643e53f9ee34253547b8e72fe452df31
Justin Locsei <justin.locsei@gmail.com>
2016-05-05 20:20:27 -0400

    Temporarily remove the API URLs

--- chiton/urls.py
+++ chiton/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
 
-from chiton.api.urls import urlpatterns as api_urls
 from chiton.core import admin
 
 urlpatterns = [
-    url(r'^api/', include(api_urls)),
     url(r'^stockroom/', admin.site.urls)
 ]
