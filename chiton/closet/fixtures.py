f3eeb4832d02d54ea36571db47dd23545b67ef89
Justin Locsei <justin.locsei@gmail.com>
2016-05-08 14:07:13 -0400

    Add a fixture for sizes

--- chiton/closet/fixtures.py
+++ chiton/closet/fixtures.py
@@ -1,9 +1,10 @@
-from chiton.closet.models import Brand, Color
+from chiton.closet.models import Brand, Color, Size
 from chiton.core.data import Fixture
 
 
 def load_fixtures():
     return [
         Fixture(Brand, Brand.objects.all()),
-        Fixture(Color, Color.objects.all())
+        Fixture(Color, Color.objects.all()),
+        Fixture(Size, Size.objects.all())
     ]
