5fcbc33cd14e7f8b95e36d6a4d14f1bf49d9bd9f
Justin Locsei <justin.locsei@gmail.com>
2016-05-08 14:13:11 -0400

    Remove the brand fixture

--- chiton/closet/fixtures.py
+++ chiton/closet/fixtures.py
@@ -1,8 +1,2 @@
-from chiton.closet.models import Brand
-from chiton.core.data import Fixture
-
-
 def load_fixtures():
-    return [
-        Fixture(Brand, Brand.objects.all())
-    ]
+    return []
