07544127d3fab371f81f828bf44964f49f7f66a7
Justin Locsei <justin.locsei@gmail.com>
2016-05-13 21:37:45 -0400

    Add a function to apply affiliate-network data to affiliate-item models

---
+++ chiton/rack/affiliates/data.py
@@ -0,0 +1,124 @@
+from chiton.closet.models import Size
+from chiton.rack.affiliates import create_affiliate
+from chiton.rack.models import ProductImage, StockRecord
+
+
+def refresh_affiliate_item(item, full=False):
+    """Refresh the data for an affiliate item based on an API query.
+
+    This sends a details query to the API of the item's affiliate network, and
+    updates the item record with the response data.  If the `full` keyword arg
+    is true, this will first re-fetch the item's GUID from its URL, and then
+    issue the details query.
+
+    Args:
+        item (chiton.rack.models.AffiliateItem): An affiliate item
+
+    Keyword Args:
+        full (bool): Whether to perform a full refresh
+
+    Returns:
+        chiton.rack.models.AffiliateItem: The updated affiliate item
+
+    Raises:
+        chiton.rack.exceptions.LookupError: If the item's information cannot be updated
+    """
+    affiliate = create_affiliate(slug=item.network.slug)
+    basic = item.garment.basic
+
+    if full:
+        overview = affiliate.request_overview(item.url)
+        item.guid = overview.guid
+        item.name = overview.name
+
+    # Place the primary color at the head of the colors list for fetching
+    # details, followed by the secondary colors
+    color_names = []
+    primary_color_name = getattr(basic.primary_color, 'name', None)
+    if primary_color_name:
+        color_names.append(primary_color_name)
+    color_names += basic.secondary_colors.values_list('name', flat=True)
+
+    details = affiliate.request_details(item.guid, colors=color_names)
+
+    item.price = details.price
+    _update_item_image(item, 'image', details.image)
+    _update_item_image(item, 'thumbnail', details.thumbnail)
+    _update_stock_records(item, details.availability)
+
+    item.save()
+
+    return item
+
+
+def _update_item_image(item, image_field, data):
+    """Update the image associated with an affiliate item.
+
+    Args:
+        item (chiton.rack.models.AffiliateItem): An affiliate item
+        image_field (str): The name of the image field on the item
+        data (dict): The API response describing the image
+    """
+    image = getattr(item, image_field)
+
+    if image:
+        image.height = data.height
+        image.width = data.width
+        image.url = data.url
+        image.save()
+    else:
+        image = ProductImage.objects.create(
+            height=data.height,
+            width=data.width,
+            url=data.url
+        )
+        setattr(item, image_field, image)
+
+
+def _update_stock_records(item, records):
+    """Update an item's stock records.
+
+    This looks at the sizes of the given stock records and maps them to known
+    canonical sizes.  If an availability record is present, a stock record is
+    created that marks the item as in-stock.  If no record is present, an
+    out-of-stock record is created instead.
+
+    If no availability information at all is provided, a stock record showing
+    the item as in-stock is created for every known size.
+
+    Args:
+        item (chiton.rack.models.AffiliateItem): An affiliate item
+        records (list): Possible ItemAvailability instances for the item
+    """
+    all_sizes = Size.objects.all()
+    existing_records = item.stock_records.all().select_related('size')
+
+    has_info = records is not None
+    record_sizes = [r.size for r in records or []]
+
+    # Build a map between Size instances and availability, as indicated by
+    # occurrences of known sizes in the given availability records
+    availability = {}
+    for size in all_sizes:
+        if has_info:
+            is_available = size.full_name in record_sizes
+        else:
+            is_available = True
+        availability[size] = is_available
+
+    for size in all_sizes:
+        current_record = None
+        for existing_record in existing_records:
+            if existing_record.size == size:
+                current_record = existing_record
+                break
+
+        if current_record:
+            current_record.is_available = availability[size]
+            current_record.save()
+        else:
+            StockRecord.objects.create(
+                item=item,
+                size=size,
+                is_available=availability[size]
+            )
---
+++ tests/unit/rack/test_affiliates_data.py
@@ -0,0 +1,230 @@
+from decimal import Decimal
+
+import mock
+import pytest
+
+from chiton.closet.models import Color, Size
+from chiton.rack.affiliates.data import refresh_affiliate_item
+from chiton.rack.affiliates.base import Affiliate
+
+
+CREATE_AFFILIATE = 'chiton.rack.affiliates.data.create_affiliate'
+
+
+class FullAffiliate(Affiliate):
+    """An affiliate that returns full API information."""
+
+    def provide_overview(self, url):
+        return {
+            'name': 'Test Name',
+            'guid': '1234'
+        }
+
+    def provide_details(self, guid, colors=[]):
+        return {
+            'availability': [
+                {'size': 'Large (8)'},
+                {'size': 'Jumbo (10)'}
+            ],
+            'image': {
+                'height': 100,
+                'url': 'http://example.com/%s' % '/'.join(colors),
+                'width': 100
+            },
+            'price': Decimal('9.99'),
+            'thumbnail': {
+                'height': 50,
+                'url': 'http://example.net/%s' % guid,
+                'width': 50
+            }
+        }
+
+
+class PartialAffiliate(Affiliate):
+    """An affiliate that does not return availability information."""
+
+    def provide_details(self, guid, colors=[]):
+        return {
+            'image': {
+                'height': 100,
+                'url': 'http://example.com/image',
+                'width': 100
+            },
+            'price': Decimal('9.99'),
+            'thumbnail': {
+                'height': 50,
+                'url': 'http://example.com/thumbnail',
+                'width': 50
+            }
+        }
+
+
+@pytest.fixture
+def affiliate_item(basic_factory, garment_factory, affiliate_item_factory):
+    white = Color.objects.create(name='White')
+    gray = Color.objects.create(name='Gray')
+    blue = Color.objects.create(name='Blue')
+
+    basic = basic_factory(primary_color=white)
+    basic.secondary_colors.add(gray)
+    basic.secondary_colors.add(blue)
+    garment = garment_factory(basic=basic)
+
+    return affiliate_item_factory(garment=garment)
+
+
+@pytest.mark.django_db
+class TestRefreshAffiliateItem:
+
+    def test_affiliate_network(self, affiliate_network_factory, affiliate_item_factory):
+        """It creates an affiliate instance using the item's network slug."""
+        network = affiliate_network_factory(slug='shopping')
+        affiliate_item = affiliate_item_factory(network=network)
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+            create_affiliate.assert_called_with(slug='shopping')
+
+    def test_network_data(self, affiliate_item):
+        """It adds detailed data to an affiliate item from the affiliate network's API."""
+        assert affiliate_item.price is None
+        assert affiliate_item.image is None
+        assert affiliate_item.thumbnail is None
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+        assert affiliate_item.price == Decimal('9.99')
+        assert affiliate_item.image.height == 100
+        assert affiliate_item.thumbnail.height == 50
+
+    def test_network_data_guid(self, affiliate_item_factory):
+        """It creates an affiliate instance using the item's network slug."""
+        affiliate_item = affiliate_item_factory(guid='4321')
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+        assert affiliate_item.thumbnail.url == 'http://example.net/4321'
+
+    def test_network_data_colors(self, affiliate_item):
+        """It fetches details using the basic's primary and secondary colors, ordered by name."""
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+        assert affiliate_item.image.url == 'http://example.com/White/Blue/Gray'
+
+    def test_network_data_colors_empty(self, affiliate_item_factory):
+        """It fetches details when no colors are associated with the basic."""
+        affiliate_item = affiliate_item_factory()
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+        assert affiliate_item.image.url == 'http://example.com/'
+
+    def test_network_data_images_idempotent(self, affiliate_item):
+        """It creates or updates images for the colors."""
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+
+            refresh_affiliate_item(affiliate_item)
+            first_image_pk = affiliate_item.image.pk
+            first_thumbnail_pk = affiliate_item.thumbnail.pk
+
+            affiliate_item.image.height = 200
+            affiliate_item.image.save()
+
+            affiliate_item.thumbnail.height = 100
+            affiliate_item.thumbnail.save()
+
+            refresh_affiliate_item(affiliate_item)
+            second_image_pk = affiliate_item.image.pk
+            second_thumbnail_pk = affiliate_item.thumbnail.pk
+
+        assert first_image_pk is not None
+        assert first_thumbnail_pk is not None
+
+        assert first_image_pk == second_image_pk
+        assert first_thumbnail_pk == second_thumbnail_pk
+
+        assert affiliate_item.image.height == 100
+        assert affiliate_item.thumbnail.height == 50
+
+    def test_network_data_stock_records(self, affiliate_item):
+        """It creates stock records for all sizes that match a known size's full display name."""
+        Size.objects.create(base='Medium', size_lower=6, size_upper=6)
+        Size.objects.create(base='Large', size_lower=8, size_upper=8)
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+        availability_by_name = {}
+        for record in affiliate_item.stock_records.all():
+            availability_by_name[record.size.full_name] = record.is_available
+
+        assert availability_by_name['Large (8)']
+        assert not availability_by_name['Medium (6)']
+        assert 'Jumbo (10)' not in availability_by_name
+
+    def test_network_data_stock_records_default(self, affiliate_item):
+        """It creates in-stock records for all known sizes if an affiliate provides no availability information."""
+        Size.objects.create(base='Small', size_lower=2, size_upper=2)
+        Size.objects.create(base='Medium', size_lower=4, size_upper=4)
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = PartialAffiliate()
+            refresh_affiliate_item(affiliate_item)
+
+        records = affiliate_item.stock_records.all()
+        assert len(records) == 2
+        assert all([r.is_available for r in records])
+
+    def test_network_data_stock_records_existing(self, affiliate_item):
+        """It updates stock records for items with existing records."""
+        Size.objects.create(base='Medium', size_lower=4, size_upper=4)
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = PartialAffiliate()
+
+            refresh_affiliate_item(affiliate_item)
+            records = affiliate_item.stock_records.all()
+
+            assert len(records) == 1
+            medium = records[0]
+            create_medium_pk = medium.pk
+
+            medium.is_available = False
+            medium.save()
+
+            refresh_affiliate_item(affiliate_item)
+            records = affiliate_item.stock_records.all()
+
+            assert len(records) == 1
+            medium = records[0]
+            update_medium_pk = medium.pk
+
+        assert create_medium_pk is not None
+        assert create_medium_pk == update_medium_pk
+
+        assert medium.is_available
+
+    def test_full_refresh(self, affiliate_item):
+        """It re-fetches the item's GUID and name when performing a full refresh."""
+        affiliate_item.guid = '4321'
+        affiliate_item.name = 'Changed'
+        affiliate_item.save()
+
+        with mock.patch(CREATE_AFFILIATE) as create_affiliate:
+            create_affiliate.return_value = FullAffiliate()
+            refresh_affiliate_item(affiliate_item, full=True)
+
+        assert affiliate_item.guid == '1234'
+        assert affiliate_item.name == 'Test Name'
