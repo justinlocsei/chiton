acf094e9870dccd7325faf8ea92fb9ffa492cedb
Justin Locsei <justin.locsei@gmail.com>
2016-05-16 16:14:28 -0400

    Simplify the public API for running batch-update jobs

--- chiton/rack/affiliates/bulk.py
+++ chiton/rack/affiliates/bulk.py
@@ -19,6 +19,110 @@ API_TIMEOUT = 1.5
 MAX_API_SLEEP = 15
 
 
+class BatchJobResult:
+    """The result of processing a single task in a batch job."""
+
+    def __init__(self, label, details=None, is_error=False):
+        """Create a new job result.
+
+        Args:
+            label (str): The label for the result
+
+        Keyword Args:
+            details (str): A detailed message describing the result
+            is_error (bool): Whether the job failed
+        """
+        self.label = label
+        self.details = details
+        self.is_error = is_error
+
+
+class BatchJob:
+    """A batch-upate job performed on a site of affiliate items."""
+
+    def __init__(self, items, processor, workers=2):
+        """Create a new batch job.
+
+        Args:
+            items (django.db.models.query.QuerySet): A queryset of affiliate items
+            item_updater (function): A function to update a single item
+
+        Keyword Args:
+            workers (int): The number of workers to use
+        """
+        self.items = items
+        self.processor = processor
+        self.workers = workers
+
+    def run(self):
+        """Run the batch job on the items.
+
+        Yields:
+            chiton.rack.affiliates.bulk.BatchJobResult: The result of processing an item
+        """
+        processor = self.processor
+        retry_range = range(1, MAX_API_RETRIES + 5)
+        queue = Queue()
+
+        def refresh_item(item):
+            label = '%s: %s' % (item.network.name, item.name)
+
+            for retry_index in retry_range:
+                try:
+                    processor(item)
+
+                # If we receive a throttling error from the API, and we have yet to
+                # exceed the maximum retries, randomly calculate a delay using an
+                # exponential backoff algorithm.  If the maximum retries have been
+                # exceeded, add an error message to the work queue.
+                except ThrottlingError:
+                    if retry_index < MAX_API_RETRIES:
+                        delay = random.uniform(1, min(MAX_API_SLEEP, API_TIMEOUT * 2 ** retry_index))
+                        sleep(delay)
+                    else:
+                        return queue.put(BatchJobResult(
+                            details='Exceeded max throttling retries of %d' % MAX_API_RETRIES,
+                            label=label,
+                            is_error=True
+                        ))
+
+                # If the API call resuled in an error of any kind, capture the
+                # error's traceback and add it as the detail message to the queue
+                except Exception:
+                    error_buffer = StringIO()
+                    print_exc(file=error_buffer)
+                    return queue.put(BatchJobResult(
+                        details=error_buffer.getvalue().strip(),
+                        label=label,
+                        is_error=True
+                    ))
+
+                # If the API call succeeded, add a success message to the queue
+                else:
+                    return queue.put(BatchJobResult(
+                        label=label,
+                        is_error=False
+                    ))
+
+        pool = ThreadPool(self.workers)
+        pool.map_async(refresh_item, self.items)
+        pool.close()
+
+        total_count = self.items.count()
+        processed_count = 0
+
+        while True:
+            yield queue.get()
+            queue.task_done()
+
+            processed_count += 1
+            if processed_count == total_count:
+                break
+
+        pool.join()
+        queue.join()
+
+
 def bulk_update_affiliate_item_metadata(items, workers=2):
     """Refresh the metadata for a batch of affiliate items.
 
@@ -29,10 +133,10 @@ def bulk_update_affiliate_item_metadata(items, workers=2):
         workers (int): The number of workers to use to process the items
 
     Returns:
-        queue.Queue: A queue that will contain all processed affiliate items
+        chiton.rack.affiliates.bulk.BatchJob: A batch job describing the updates
     """
     items = items.select_related('network')
-    return _bulk_update_items(items, update_affiliate_item_metadata, workers)
+    return BatchJob(items, update_affiliate_item_metadata, workers=workers)
 
 
 def bulk_update_affiliate_item_details(items, workers=2):
@@ -45,68 +149,7 @@ def bulk_update_affiliate_item_details(items, workers=2):
         workers (int): The number of workers to use to process the items
 
     Returns:
-        queue.Queue: A queue that will contain all processed affiliate items
+        chiton.rack.affiliates.bulk.BatchJob: A batch job describing the updates
     """
     items = items.select_related('garment__basic', 'image', 'thumbnail', 'network')
-    return _bulk_update_items(items, update_affiliate_item_details, workers)
-
-
-def _bulk_update_items(items, item_updater, workers):
-    """Create a function that can be used for bulk updates of an item.
-
-    Args:
-        items (django.db.models.query.QuerySet): A queryset of affiliate items
-        item_updater (function): A function to update a single item
-        workers (int): The number of workers to use
-
-    Returns:
-        queue.Queue: A queue that will contain information on processed items
-    """
-    retry_range = range(1, MAX_API_RETRIES + 5)
-    queue = Queue()
-
-    def refresh_item(item):
-        label = '%s: %s' % (item.network.name, item.name)
-
-        for retry_index in retry_range:
-            try:
-                item_updater(item)
-
-            # If we receive a throttling error from the API, and we have yet to
-            # exceed the maximum retries, randomly calculate a delay using an
-            # exponential backoff algorithm.  If the maximum retries have been
-            # exceeded, add an error message to the work queue.
-            except ThrottlingError:
-                if retry_index < MAX_API_RETRIES:
-                    delay = random.uniform(1, min(MAX_API_SLEEP, API_TIMEOUT * 2 ** retry_index))
-                    sleep(delay)
-                else:
-                    return queue.put({
-                        'details': 'Exceeded max throttling retries of %d' % MAX_API_RETRIES,
-                        'label': label,
-                        'is_error': True
-                    })
-
-            # If the API call resuled in an error of any kind, capture the
-            # error's traceback and add it as the detail message to the queue
-            except Exception:
-                error_buffer = StringIO()
-                print_exc(file=error_buffer)
-                return queue.put({
-                    'details': error_buffer.getvalue().strip(),
-                    'label': label,
-                    'is_error': True
-                })
-
-            # If the API call succeeded, add a success message to the queue
-            else:
-                return queue.put({
-                    'label': label,
-                    'is_error': False
-                })
-
-    pool = ThreadPool(workers)
-    pool.map_async(refresh_item, items)
-    pool.close()
-
-    return queue
+    return BatchJob(items, update_affiliate_item_details, workers=workers)
--- chiton/rack/management/commands/chiton_refresh_affiliate_items.py
+++ chiton/rack/management/commands/chiton_refresh_affiliate_items.py
@@ -29,10 +29,6 @@ class Command(BaseCommand):
         items = AffiliateItem.objects.all().order_by('pk')
         total_count = items.count()
 
-        processed_count = 0
-        error_count = 0
-        failed_updates = []
-
         if total_count == 0:
             self.stdout.write('No affiliate items exist')
             return
@@ -41,32 +37,25 @@ class Command(BaseCommand):
             self.stdout.write('Updating %s for %d items with %d workers\n--' % (target_noun, total_count, options['workers']))
 
         if options['meta']:
-            item_updater = bulk_update_affiliate_item_metadata
+            create_batch_job = bulk_update_affiliate_item_metadata
         else:
-            item_updater = bulk_update_affiliate_item_details
+            create_batch_job = bulk_update_affiliate_item_details
 
-        item_queue = item_updater(items, workers=options['workers'])
+        batch_job = create_batch_job(items, workers=options['workers'])
+        error_count = 0
+        failed_updates = []
 
-        while True:
-            task = item_queue.get()
-            item_queue.task_done()
-            processed_count += 1
-
-            if task['is_error']:
+        for index, result in enumerate(batch_job.run()):
+            if result.is_error:
                 error_count += 1
-                self.stdout.write(self.style.ERROR('\n[!] %d/%d (%s)' % (processed_count, total_count, task['label'])))
-                self.stdout.write(self.style.ERROR('--\n%s\n--\n' % task['details']))
-                failed_updates.append(task['label'])
+                self.stdout.write(self.style.ERROR('\n[!] %d/%d (%s)' % (index + 1, total_count, result.label)))
+                self.stdout.write(self.style.ERROR('--\n%s\n--\n' % result.details))
+                failed_updates.append(result.label)
             else:
-                self.stdout.write('%d/%d (%s)' % (processed_count, total_count, task['label']))
-
-            if processed_count == total_count:
-                break
-
-        item_queue.join()
+                self.stdout.write('%d/%d (%s)' % (index + 1, total_count, result.label))
 
         if error_count:
-            self.stdout.write(self.style.ERROR('\nUpdated %d/%d items' % (processed_count - error_count, total_count)))
+            self.stdout.write(self.style.ERROR('\nUpdated %d/%d items' % (total_count - error_count, total_count)))
             self.stdout.write(self.style.ERROR('%d items could not be updated' % error_count))
             for failed_update in failed_updates:
                 self.stdout.write(self.style.ERROR('* %s' % failed_update))
