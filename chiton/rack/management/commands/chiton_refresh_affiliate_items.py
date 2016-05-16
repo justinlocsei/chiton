2708b5e37e47029b578a31423c8c4096213160d7
Justin Locsei <justin.locsei@gmail.com>
2016-05-15 20:54:13 -0400

    Use more verbose output when showing failed affiliate-item updates

--- chiton/rack/management/commands/chiton_refresh_affiliate_items.py
+++ chiton/rack/management/commands/chiton_refresh_affiliate_items.py
@@ -28,7 +28,10 @@ class Command(BaseCommand):
     def handle(self, *arg, **options):
         items = AffiliateItem.objects.all().order_by('pk')
         total_count = items.count()
+
         processed_count = 0
+        error_count = 0
+        failed_updates = []
 
         if total_count == 0:
             self.stdout.write('No affiliate items exist')
@@ -50,8 +53,10 @@ class Command(BaseCommand):
             processed_count += 1
 
             if task['is_error']:
+                error_count += 1
                 self.stdout.write(self.style.ERROR('\n[!] %d/%d (%s)' % (processed_count, total_count, task['label'])))
                 self.stdout.write(self.style.ERROR('--\n%s\n--\n' % task['details']))
+                failed_updates.append(task['label'])
             else:
                 self.stdout.write('%d/%d (%s)' % (processed_count, total_count, task['label']))
 
@@ -59,4 +64,11 @@ class Command(BaseCommand):
                 break
 
         item_queue.join()
-        self.stdout.write('\nAll affiliate items updated!')
+
+        if error_count:
+            self.stdout.write(self.style.ERROR('\nUpdated %d/%d items' % (processed_count - error_count, total_count)))
+            self.stdout.write(self.style.ERROR('%d items could not be updated' % error_count))
+            for failed_update in failed_updates:
+                self.stdout.write(self.style.ERROR('* %s' % failed_update))
+        else:
+            self.stdout.write(self.style.SUCCESS('\nUpdated all %d items' % (total_count)))
