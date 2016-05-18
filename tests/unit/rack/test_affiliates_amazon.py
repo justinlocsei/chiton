03f169a5e6259fd0afa232e3de202339d23dc606
Justin Locsei <justin.locsei@gmail.com>
2016-05-17 20:51:27 -0400

    Simplify the affiliate API tests

--- tests/unit/rack/test_affiliates_amazon.py
+++ tests/unit/rack/test_affiliates_amazon.py
@@ -10,53 +10,41 @@ class TestAmazonAffiliate:
 
     def test_request_overview_valid_asin(self, amazon_api_request):
         """It returns a name and GUID when using a URL with a valid ASIN."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
-            overview = affiliate.request_overview('http://www.amazon.com/dp/B00ZGRB7S6')
+            overview = Affiliate().request_overview('http://www.amazon.com/dp/B00ZGRB7S6')
 
         assert overview.guid == 'B00ZGRB7S6'
         assert overview.name == 'Tahari by ASL Baron Short Sleeve A-Line Dress, Red'
 
     def test_request_overview_valid_asin_parent_asin(self, amazon_api_request):
         """It returns the parent ASIN when given the URL for a child item."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
-            overview = affiliate.request_overview('http://www.amazon.com/dp/B00ZGRB7UO')
+            overview = Affiliate().request_overview('http://www.amazon.com/dp/B00ZGRB7UO')
 
         assert overview.guid == 'B00ZGRB7S6'
         assert overview.name == 'Tahari by ASL Baron Short Sleeve A-Line Dress, Red (16)'
 
     def test_request_overview_invalid_asin(self, amazon_api_request):
         """It raises an error when an invalid ASIN is used."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_overview('http://www.amazon.com/dp/0000000000')
+                Affiliate().request_overview('http://www.amazon.com/dp/0000000000')
 
     def test_request_overview_invalid_url(self):
         """It raises an error when an invalid URL is used."""
-        affiliate = Affiliate()
-
         with pytest.raises(LookupError):
-            affiliate.request_overview('http://www.amazon.com')
+            Affiliate().request_overview('http://www.amazon.com')
 
     def test_request_overview_throttling(self, amazon_api_request):
         """It raises a throttling error in response to an HTTP 503 code."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(ThrottlingError):
-                affiliate.request_overview('http://www.amazon.com/dp/B00ZGRB7S6')
+                Affiliate().request_overview('http://www.amazon.com/dp/B00ZGRB7S6')
 
     def test_request_details_name(self, amazon_api_request):
         """It returns the item's name."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
-            details = affiliate.request_details('B00ZGRB7S6')
+            details = Affiliate().request_details('B00ZGRB7S6')
 
         assert details.name == 'Tahari by ASL Baron Short Sleeve A-Line Dress, Red'
 
@@ -79,10 +67,8 @@ class TestAmazonAffiliate:
 
     def test_request_details_image(self, amazon_api_request):
         """It returns information on the item's images."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
-            details = affiliate.request_details('B00YJJ4SNS')
+            details = Affiliate().request_details('B00YJJ4SNS')
 
         assert '.jpg' in details.image.url
         assert '.jpg' in details.thumbnail.url
@@ -130,58 +116,44 @@ class TestAmazonAffiliate:
 
     def test_request_details_availability(self, amazon_api_request):
         """It marks every item as globally available."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
-            details = affiliate.request_details('B00ZGRB7S6')
+            details = Affiliate().request_details('B00ZGRB7S6')
 
         assert details.availability is True
 
     def test_request_details_valid_asin_child(self, amazon_api_request):
         """It raises an error when requesting details for a child item."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_details('B00ZGRB7UO')
+                Affiliate().request_details('B00ZGRB7UO')
 
     def test_request_details_invalid_asin(self, amazon_api_request):
         """It raises an error when looking up details for an inavlid ASIN."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_details('0000000000')
+                Affiliate().request_details('0000000000')
 
     def test_request_details_throttling(self, amazon_api_request):
         """It raises a throttling error in response to an HTTP 503 code."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(ThrottlingError):
-                affiliate.request_details('B00ZGRB7S6')
+                Affiliate().request_details('B00ZGRB7S6')
 
     def test_request_raw(self, amazon_api_request):
         """It returns the full API response."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
-            raw = affiliate.request_raw('B00ZGRB7UO')
+            raw = Affiliate().request_raw('B00ZGRB7UO')
 
         assert raw['Item']['ASIN'] == 'B00ZGRB7UO'
 
     def test_request_raw_invalid_asin(self, amazon_api_request):
         """It raises an error when getting a raw response for an inavlid ASIN."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_raw('0000000000')
+                Affiliate().request_raw('0000000000')
 
     def test_request_raw_throttling(self, amazon_api_request):
         """It raises a throttling error in response to an HTTP 503 code."""
-        affiliate = Affiliate()
-
         with amazon_api_request():
             with pytest.raises(ThrottlingError):
-                affiliate.request_raw('B00ZGRB7S6')
+                Affiliate().request_raw('B00ZGRB7S6')
--- tests/unit/rack/test_affiliates_shopstyle.py
+++ tests/unit/rack/test_affiliates_shopstyle.py
@@ -10,53 +10,41 @@ class TestShopstyleAffiliate:
 
     def test_request_overview_valid_product_id(self, shopstyle_api_request):
         """It returns a name and GUID when using a URL with a valid product ID."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            overview = affiliate.request_overview('http://api.shopstyle.com/action/apiVisitRetailer?id=471281504')
+            overview = Affiliate().request_overview('http://api.shopstyle.com/action/apiVisitRetailer?id=471281504')
 
         assert overview.guid == '471281504'
         assert overview.name == 'J.Crew Double-breasted blazer'
 
     def test_request_overview_invalid_product_id(self, shopstyle_api_request):
         """It raises an error when an invalid product ID is used."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_overview('http://api.shopstyle.com/action/apiVisitRetailer?id=000000000')
+                Affiliate().request_overview('http://api.shopstyle.com/action/apiVisitRetailer?id=000000000')
 
     def test_request_overview_invalid_url(self):
         """It raises an error when an invalid URL is used."""
-        affiliate = Affiliate()
-
         with pytest.raises(LookupError):
-            affiliate.request_overview('http://www.shopstyle.com')
+            Affiliate().request_overview('http://www.shopstyle.com')
 
     def test_request_details_name(self, shopstyle_api_request):
         """It returns the item's name in its details."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            details = affiliate.request_details('471281504')
+            details = Affiliate().request_details('471281504')
 
         assert details.name == 'J.Crew Double-breasted blazer'
 
     def test_request_details_price(self, shopstyle_api_request):
         """It returns the item's listed price in its details."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            details = affiliate.request_details('471281504')
+            details = Affiliate().request_details('471281504')
 
         assert details.price == Decimal('198.00')
 
     def test_request_details_image(self, shopstyle_api_request):
         """It returns a primary and thumbnail image for the item."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            details = affiliate.request_details('471281504')
+            details = Affiliate().request_details('471281504')
 
         assert '.jpg' in details.image.url
         assert '.jpg' in details.thumbnail.url
@@ -102,10 +90,8 @@ class TestShopstyleAffiliate:
 
     def test_request_details_availability(self, shopstyle_api_request):
         """It returns unique availability information based off of the canonical sizes."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            details = affiliate.request_details('470750142')
+            details = Affiliate().request_details('470750142')
 
         assert details.availability is not None
 
@@ -146,43 +132,33 @@ class TestShopstyleAffiliate:
 
     def test_request_details_availability_no_stock_records(self, shopstyle_api_request):
         """It signals global availability when no stock information is present but the item is marked as in stock."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            details = affiliate.request_details('493153459')
+            details = Affiliate().request_details('493153459')
 
         assert details.availability is True
 
     def test_request_details_availability_partial_stock_records(self, shopstyle_api_request):
         """It signals global availability when an item's stock records lack a size and color and the item is marked as in stock."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            details = affiliate.request_details('494093141')
+            details = Affiliate().request_details('494093141')
 
         assert details.availability is True
 
     def test_request_details_invalid_product_id(self, shopstyle_api_request):
         """It raises an error when looking up details for an inavlid product ID."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_details('0000000000')
+                Affiliate().request_details('0000000000')
 
     def test_request_raw(self, shopstyle_api_request):
         """It returns the full API response."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
-            raw = affiliate.request_raw('471281504')
+            raw = Affiliate().request_raw('471281504')
 
         assert raw['id'] == 471281504
 
     def test_request_raw_invalid_asin(self, shopstyle_api_request):
         """It raises an error when getting a raw response for an inavlid ASIN."""
-        affiliate = Affiliate()
-
         with shopstyle_api_request():
             with pytest.raises(LookupError):
-                affiliate.request_raw('0000000000')
+                Affiliate().request_raw('0000000000')
