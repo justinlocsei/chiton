19ae8be7b82548e252bd44cdcca0aa45e2b15f44
Justin Locsei <justin.locsei@gmail.com>
2016-05-24 20:12:43 -0400

    Add a model field for storing prices

--- chiton/closet/model_fields.py
+++ chiton/closet/model_fields.py
@@ -15,3 +15,16 @@ class EmphasisField(models.SmallIntegerField):
             kwargs['default'] = EMPHASES['NEUTRAL']
 
         super().__init__(*args, **kwargs)
+
+
+class PriceField(models.DecimalField):
+    """A field for storing a price for an article of clothing."""
+
+    description = _('A price')
+
+    def __init__(self, *args, **kwargs):
+        options = {}
+        options.update({'max_digits': 8, 'decimal_places': 2})
+        options.update(kwargs)
+
+        super().__init__(*args, **options)
