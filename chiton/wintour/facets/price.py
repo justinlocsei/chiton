505adb9063e4db35371777c06356540690e30eb9
Justin Locsei <justin.locsei@gmail.com>
2016-05-24 21:20:03 -0400

    Implement the price facet

--- chiton/wintour/facets/price.py
+++ chiton/wintour/facets/price.py
@@ -1,4 +1,11 @@
+from decimal import Decimal
+
+from chiton.runway.models import Basic
 from chiton.wintour.facets import BaseFacet
+
+
+# The order of the prices groups, by slug
+PRICE_GROUP_ORDER = ('budget', 'normal', 'luxury')
 
 
 class PriceFacet(BaseFacet):
@@ -6,3 +13,57 @@ class PriceFacet(BaseFacet):
 
     name = 'Price'
     slug = 'price'
+
+    def provide_profile_data(self, profile):
+        basic_prices = {}
+        for basic in Basic.objects.all():
+            basic_prices[basic.pk] = {
+                'budget': basic.budget_end or Decimal(0),
+                'luxury': basic.luxury_start or Decimal(0)
+            }
+
+        return {
+            'basic_prices': basic_prices
+        }
+
+    def apply(self, basic, garments, basic_prices=None):
+        cutoffs = basic_prices[basic.pk]
+
+        groups = {
+            'budget': [],
+            'normal': [],
+            'luxury': []
+        }
+
+        # Place items with prices into one of the groups, based on where the
+        # item's price falls relative to the basic's cutoff points
+        for garment in garments:
+            priced_items = [ai for ai in garment['affiliate_items'] if ai.price]
+            total_price = sum([pi.price for pi in priced_items])
+            if not total_price:
+                continue
+
+            garment_price = total_price / len(priced_items)
+            if garment_price <= cutoffs['budget']:
+                group_name = 'budget'
+            elif garment_price >= cutoffs['luxury']:
+                group_name = 'luxury'
+            else:
+                group_name = 'normal'
+
+            groups[group_name].append({
+                'id': garment['garment'].pk,
+                'price': garment_price,
+            })
+
+        # Create a dict for each group that exposes the IDs of its garments
+        facets = []
+        for group_slug in PRICE_GROUP_ORDER:
+            items = groups[group_slug]
+            facets.append({
+                'count': len(items),
+                'items': [i['id'] for i in items],
+                'slug': group_slug
+            })
+
+        return facets
