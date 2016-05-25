c126adae44648fdd23ef0a6195a70169ae8111cc
Justin Locsei <justin.locsei@gmail.com>
2016-05-24 21:20:18 -0400

    Show price groupings in the visualizer

--- chiton/wintour/static/wintour/js/wintour/pipeline.js
+++ chiton/wintour/static/wintour/js/wintour/pipeline.js
@@ -92,28 +92,44 @@ PipelineVisualizer.prototype = {
 
             if (!state.filters[basicSlug]) { continue; }
 
-            var garments = [];
-            var numGarments = data.garments.length;
-            for (var j = 0; j < numGarments; j++) {
-                if (j >= state.cutoff) { break; }
+            // Build a lookup table mapping garment records to IDs
+            var garmentsByID = data.garments.reduce(function(previous, garment) {
+                previous[garment.garment.id] = garment;
+                return previous;
+            }, {});
 
-                var garment = data.garments[j];
-                var weight = (garment.weight * WEIGHT_BASE).toFixed(1);
-                weight = weight.replace(/\.0$/, '');
+            // Provide data on each price group that contains rendered garments
+            var priceGroups = data.facets.price.map(function(priceGroup) {
+                var garments = [];
+                var garmentIDs = priceGroup.items;
+                var numGarments = Math.min(data.garments.length, garmentIDs.length);
 
-                garments.push(renderTemplate('pipeline-template-garment', {
-                    brand: garment.garment.brand,
-                    editURL: garment.edit_url,
-                    id: garment.garment.id,
-                    name: garment.garment.name,
-                    weight: weight
-                }));
-            }
+                for (var j = 0; j < numGarments; j++) {
+                    if (j >= state.cutoff) { break; }
+
+                    var garment = garmentsByID[garmentIDs[j]];
+                    var weight = (garment.weight * WEIGHT_BASE).toFixed(1);
+                    weight = weight.replace(/\.0$/, '');
+
+                    garments.push(renderTemplate('pipeline-template-garment', {
+                        brand: garment.garment.brand,
+                        editURL: garment.edit_url,
+                        id: garment.garment.id,
+                        name: garment.garment.name,
+                        weight: weight
+                    }));
+                }
+
+                return {
+                    garments: garments,
+                    name: priceGroup.slug
+                };
+            });
 
             var basic = data.basic;
             basics.push({
-                garments: garments,
                 name: basic.name,
+                priceGroups: priceGroups,
                 slug: basic.slug
             });
         }
