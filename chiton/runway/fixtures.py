c5673dab773509a8cc6123e00edf90a04e4c6f33
Justin Locsei <justin.locsei@gmail.com>
2016-05-27 20:02:48 -0400

    Alphabetize fixtures

--- chiton/runway/fixtures.py
+++ chiton/runway/fixtures.py
@@ -6,6 +6,6 @@ def load_fixtures():
     return [
         Fixture(Category, Category.objects.all()),
         Fixture(Formality, Formality.objects.all()),
-        Fixture(Style, Style.objects.all()),
         Fixture(Propriety, Propriety.objects.for_export()),
+        Fixture(Style, Style.objects.all())
     ]
