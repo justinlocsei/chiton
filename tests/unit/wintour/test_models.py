e70f9110fa91c50a08015866627e89fbeb656bc5
Justin Locsei <justin.locsei@gmail.com>
2016-05-27 21:24:13 -0400

    Add test for the Person model

---
+++ tests/unit/wintour/test_models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+import pytest
+
+from chiton.wintour.models import Person
+
+
+@pytest.fixture
+def john_doe():
+    return User.objects.create_user('jdoe', 'jdoe@example.com')
+
+
+@pytest.mark.django_db
+class TestPerson:
+
+    def test_full_name(self, john_doe):
+        """It builds the person's full name from their first and last name."""
+        person = Person.objects.create(first_name="John", last_name="Doe", user=john_doe)
+
+        assert person.full_name == 'John Doe'
+
+    def test_slug(self, john_doe):
+        """It uses the person's full name as the slug."""
+        person = Person.objects.create(first_name="John", last_name="Doe", user=john_doe)
+
+        assert person.slug == 'john-doe'
