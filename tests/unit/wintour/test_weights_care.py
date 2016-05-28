b543a83a797872d7e2671540924b1932a06284c3
Justin Locsei <justin.locsei@gmail.com>
2016-05-28 19:26:34 -0400

    Add tests for the care-type weight

---
+++ tests/unit/wintour/test_weights_care.py
@@ -0,0 +1,80 @@
+import pytest
+
+from chiton.closet.data import CARE_TYPES
+from chiton.wintour.pipeline import PipelineProfile
+from chiton.wintour.weights.care import CareWeight
+
+
+@pytest.mark.django_db
+class TestCareWeight:
+
+    def test_no_matches(self, garment_factory):
+        """It returns a null weight when a garment's care type is not blacklisted."""
+        garment = garment_factory(care=CARE_TYPES['HAND_WASH'])
+        profile = PipelineProfile(avoid_care=[CARE_TYPES['DRY_CLEAN']])
+
+        weight = CareWeight()
+        with weight.apply_to_profile(profile) as apply_fn:
+            result = apply_fn(garment)
+            assert not result
+
+    def test_blacklist(self, garment_factory):
+        """It uses a negative weight when a garment's care type is blacklisted."""
+        garment = garment_factory(care=CARE_TYPES['DRY_CLEAN'])
+        profile = PipelineProfile(avoid_care=[CARE_TYPES['DRY_CLEAN']])
+
+        weight = CareWeight()
+        with weight.apply_to_profile(profile) as apply_fn:
+            result = apply_fn(garment)
+            assert result < 0
+
+    def test_blacklist_multiple(self, garment_factory):
+        """It accepts multiple values to blacklist."""
+        garment = garment_factory(care=CARE_TYPES['DRY_CLEAN'])
+        profile = PipelineProfile(avoid_care=[CARE_TYPES['DRY_CLEAN'], CARE_TYPES['HAND_WASH']])
+
+        weight = CareWeight()
+        with weight.apply_to_profile(profile) as apply_fn:
+            result = apply_fn(garment)
+            assert result < 0
+
+    def test_blacklist_machine(self, garment_factory):
+        """It ignores blacklist requests for machine-wash garments."""
+        garment_machine_machine = garment_factory(care=CARE_TYPES['MACHINE_MACHINE'])
+        garment_machine_air = garment_factory(care=CARE_TYPES['MACHINE_AIR'])
+        profile = PipelineProfile(avoid_care=[CARE_TYPES['MACHINE_MACHINE'], CARE_TYPES['MACHINE_AIR']])
+
+        weight = CareWeight()
+        with weight.apply_to_profile(profile) as apply_fn:
+            machine_machine_result = apply_fn(garment_machine_machine)
+            machine_air_result = apply_fn(garment_machine_air)
+
+            assert not machine_machine_result
+            assert not machine_air_result
+
+    def test_debug(self, garment_factory):
+        """It logs explanations for any garments with blacklisted care types."""
+        garment_blacklist = garment_factory(care=CARE_TYPES['DRY_CLEAN'])
+        garment_normal = garment_factory(care=CARE_TYPES['HAND_WASH'])
+        profile = PipelineProfile(avoid_care=[CARE_TYPES['DRY_CLEAN']])
+
+        weight = CareWeight()
+        weight.debug = True
+
+        with weight.apply_to_profile(profile) as apply_fn:
+            apply_fn(garment_blacklist)
+            apply_fn(garment_normal)
+
+        assert weight.get_explanations(garment_blacklist)
+        assert not weight.get_explanations(garment_normal)
+
+    def test_debug_flag(self, garment_factory):
+        """It does not log explanations when debug mode is not enabled."""
+        garment = garment_factory(care=CARE_TYPES['DRY_CLEAN'])
+        profile = PipelineProfile(avoid_care=[CARE_TYPES['DRY_CLEAN']])
+
+        weight = CareWeight()
+        with weight.apply_to_profile(profile) as apply_fn:
+            apply_fn(garment)
+
+        assert not weight.get_explanations(garment)
