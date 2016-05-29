85e00ba4569f6089498b48841d1669317e141db4
Justin Locsei <justin.locsei@gmail.com>
2016-05-28 20:28:40 -0400

    Add tests for the base garment filter

---
+++ tests/unit/wintour/test_garment_filters.py
@@ -0,0 +1,21 @@
+import pytest
+
+from chiton.wintour.garment_filters import BaseGarmentFilter
+
+
+class TestFilter(BaseGarmentFilter):
+    name = 'Test'
+    slug = 'test'
+
+
+@pytest.mark.django_db
+class TestBaseGarmentFilter:
+
+    def test_apply_default(self, garment_factory):
+        """It marks a garment as non-excluded by default."""
+        garment = garment_factory()
+
+        garment_filter = TestFilter()
+        result = garment_filter.apply(garment)
+
+        assert result is False
