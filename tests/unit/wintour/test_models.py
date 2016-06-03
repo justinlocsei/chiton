c1cc833b31f0289e7fdcad94b14c330191a0c725
Justin Locsei <justin.locsei@gmail.com>
2016-06-03 08:37:32 -0400

    Condense tests

--- tests/unit/wintour/test_models.py
+++ tests/unit/wintour/test_models.py
@@ -13,12 +13,10 @@ class TestPerson:
 
     def test_full_name(self, john_doe):
         """It builds the person's full name from their first and last name."""
-        person = Person.objects.create(first_name="John", last_name="Doe", user=john_doe)
-
+        person = Person.objects.create(first_name='John', last_name='Doe', user=john_doe)
         assert person.full_name == 'John Doe'
 
     def test_slug(self, john_doe):
         """It uses the person's full name as the slug."""
-        person = Person.objects.create(first_name="John", last_name="Doe", user=john_doe)
-
+        person = Person.objects.create(first_name='John', last_name='Doe', user=john_doe)
         assert person.slug == 'john-doe'
