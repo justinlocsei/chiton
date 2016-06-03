cc12fb6e806a45d9f121138b44447d65c24f71ea
Justin Locsei <justin.locsei@gmail.com>
2016-06-03 08:26:06 -0400

    Use single quotes

--- tests/unit/closet/test_models.py
+++ tests/unit/closet/test_models.py
@@ -9,7 +9,7 @@ class TestBrand:
 
     def test_natural_key(self):
         """It uses its slug."""
-        brand = Brand.objects.create(name="Chanel", slug="chanel", age_lower=20, age_upper=30)
+        brand = Brand.objects.create(name='Chanel', slug='chanel', age_lower=20, age_upper=30)
         assert brand.natural_key() == ('chanel',)
 
         found = Brand.objects.get_by_natural_key('chanel')
@@ -17,7 +17,7 @@ class TestBrand:
 
     def test_clean_age_range(self):
         """It requires a properly ordered age range."""
-        brand = Brand(name="Ann Taylor")
+        brand = Brand(name='Ann Taylor')
         brand.age_lower = 50
         brand.age_upper = 25
 
@@ -33,7 +33,7 @@ class TestColor:
 
     def test_natural_key(self):
         """It uses its slug."""
-        color = Color.objects.create(name="Black", slug="black")
+        color = Color.objects.create(name='Black', slug='black')
         assert color.natural_key() == ('black',)
 
         found = Color.objects.get_by_natural_key('black')
@@ -46,9 +46,9 @@ class TestGarment:
     def test_natural_key(self, basic_factory, brand_factory):
         """It uses its slug and the slug of its brand."""
         basic = basic_factory()
-        brand = brand_factory(name="Givenchy", slug="givenchy")
+        brand = brand_factory(name='Givenchy', slug='givenchy')
 
-        garment = Garment.objects.create(name="Cocktail Dress", slug="cocktail-dress", brand=brand, basic=basic)
+        garment = Garment.objects.create(name='Cocktail Dress', slug='cocktail-dress', brand=brand, basic=basic)
         assert garment.natural_key() == ('cocktail-dress', 'givenchy')
 
         found = Garment.objects.get_by_natural_key('cocktail-dress', 'givenchy')
@@ -56,7 +56,7 @@ class TestGarment:
 
     def test_size_validation(self, basic_factory, brand_factory):
         """It requires at least one size to selected."""
-        garment = Garment(name="Ball Gown", brand=brand_factory(), basic=basic_factory())
+        garment = Garment(name='Ball Gown', brand=brand_factory(), basic=basic_factory())
 
         garment.is_regular_sized = False
         garment.is_plus_sized = False
