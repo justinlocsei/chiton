d76726392ad4350833085746e93a2dd8543926a7
Justin Locsei <justin.locsei@gmail.com>
2016-06-03 21:23:51 -0400

    Add a test for the fixture class

---
+++ tests/unit/core/test_fixture.py
@@ -0,0 +1,56 @@
+import json
+import tempfile
+
+from django.test import override_settings
+import mock
+import pytest
+
+from chiton.core.exceptions import FilesystemError
+from chiton.core.fixture import Fixture
+from chiton.rack.models import ProductImage
+
+
+@pytest.mark.django_db
+class TestFixture:
+
+    @pytest.fixture
+    def product_images_fixture(self, product_image_factory):
+        product_image_factory()
+        product_images = ProductImage.objects.all()
+
+        return Fixture(ProductImage, product_images)
+
+    def test_label(self, product_images_fixture):
+        """It creates a label using the underscored version of its model's name."""
+        assert product_images_fixture.label == 'product_image'
+
+    def test_find(self, product_images_fixture):
+        """It returns the absolute path to the fixture's file using the app label and fixture label."""
+        with override_settings(CHITON_ROOT='/tmp'):
+            path = product_images_fixture.find()
+            assert path == '/tmp/rack/fixtures/product_image.json'
+
+    def test_export(self, product_images_fixture):
+        """It writes a queryset's models to a JSON file."""
+        with tempfile.NamedTemporaryFile() as output:
+            product_images_fixture.find = mock.Mock(return_value=output.name)
+
+            exported = product_images_fixture.export()
+            assert exported == output.name
+
+            with open(exported) as fixture_file:
+                data = json.load(fixture_file)
+
+            assert len(data) == 1
+
+            image = data[0]
+            assert 'fields' in image
+            assert 'model' in image
+            assert 'pk' in image
+
+    def test_export_invalid_target(self, product_images_fixture):
+        """It raises an error when the target directory does not exist."""
+        product_images_fixture.find = mock.Mock(return_value='/abcdefghijklmnop/qrstuvwxyz.json')
+
+        with pytest.raises(FilesystemError):
+            product_images_fixture.export()
