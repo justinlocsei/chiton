abdf3e26389e923640550b72be486e88d98180c6
Justin Locsei <justin.locsei@gmail.com>
2016-06-09 20:15:59 -0400

    Expose all cached pipeline queries when wintour loads

--- chiton/wintour/apps.py
+++ chiton/wintour/apps.py
@@ -7,3 +7,7 @@ class Config(AppConfig):
     name = 'chiton.wintour'
     label = 'chiton_wintour'
     verbose_name = _('Wintour')
+
+    def ready(self):
+        """Import all code that uses cached queries."""
+        from chiton.wintour.pipelines.core import CorePipeline
