bb4c41a8a7e528209d5bf71f0f477de47bfe5a74
Justin Locsei <justin.locsei@gmail.com>
2016-06-15 20:34:11 -0400

    Only show the API browser when not debugging

--- chiton/settings.py
+++ chiton/settings.py
@@ -181,3 +181,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+if not DEBUG:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+        'rest_framework.renderers.JSONRenderer',
+    )
