c23be6fb5787d11a6bc083f3711e93dbef3733a8
Justin Locsei <justin.locsei@gmail.com>
2016-06-17 01:48:57 -0400

    Reduce duplication in the recommender management command

--- chiton/api/management/commands/chiton_ensure_recommender_exists.py
+++ chiton/api/management/commands/chiton_ensure_recommender_exists.py
@@ -2,14 +2,7 @@ from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
 from chiton.api.users import bind_user_to_group, enforce_auth_token, grant_group_permission
-from chiton.wintour.models import Recommendation, WardrobeProfile
-
-
-# The name of the recommeders group
-GROUP_NAME = 'Recommenders'
-
-# The models that a recommender should be able to create
-CREATE_MODELS = [Recommendation, WardrobeProfile]
+from chiton.api.permissions import IsRecommender
 
 
 class Command(BaseCommand):
@@ -20,11 +13,11 @@ class Command(BaseCommand):
         parser.add_argument('--username', type=str, required=True)
 
     def handle(self, *arg, **options):
-        group, created_group = Group.objects.get_or_create(name=GROUP_NAME)
+        group, created_group = Group.objects.get_or_create(name='Recommenders')
         if created_group:
             self.stdout.write('Created the %s group' % group.name)
 
-        for model_class in CREATE_MODELS:
+        for model_class in IsRecommender.CREATED_MODELS:
             if grant_group_permission(group, model_class, add=True):
                 self.stdout.write('Granted create permissions for %s' % model_class._meta.verbose_name_plural)
 
