b90b1b2ceb563a158f776789d20aba67c6a232e5
Justin Locsei <justin.locsei@gmail.com>
2016-06-23 10:25:26 -0400

    Serialize a subset of fields for the basic fixture

--- chiton/runway/fixtures.py
+++ chiton/runway/fixtures.py
@@ -3,9 +3,18 @@ from chiton.closet.models import Color
 from chiton.runway.models import Basic, Category, Formality, Propriety, Style
 
 
+BASIC_FIELDS = [
+    'category',
+    'name',
+    'primary_color',
+    'secondary_colors',
+    'slug'
+]
+
+
 def load_fixtures():
     return [
-        Fixture(Basic, initial=True, requires=[Category, Color]),
+        Fixture(Basic, initial=True, requires=[Category, Color], fields=BASIC_FIELDS),
         Fixture(Category),
         Fixture(Formality),
         Fixture(Propriety, queryset=Propriety.objects.for_export(), requires=[Basic, Formality]),
