0407e39ae5b8e3ccd4b151c569d4e72a0384a747
Justin Locsei <justin.locsei@gmail.com>
2016-07-11 19:43:35 -0400

    Fix failing fixture tests by swapping models

--- tests/unit/core/test_fixture.py
+++ tests/unit/core/test_fixture.py
@@ -6,58 +6,58 @@ import pytest
 
 from chiton.core.exceptions import FilesystemError
 from chiton.core.fixture import Fixture
-from chiton.rack.models import ItemImage
+from chiton.runway.models import Style
 
 
 @pytest.mark.django_db
 class TestFixture:
 
     @pytest.fixture
-    def item_images_fixture(self, item_image_factory):
-        item_image_factory()
-        item_images = ItemImage.objects.all()
+    def styles_fixture(self, style_factory):
+        style_factory()
+        styles = Style.objects.all()
 
-        return Fixture(ItemImage, item_images)
+        return Fixture(Style, styles)
 
-    def test_label(self, item_images_fixture):
-        """It creates a label using the underscored version of its model's name."""
-        assert item_images_fixture.label == 'item_image'
+    def test_label(self, styles_fixture):
+        """It creates a label using from the model's name."""
+        assert styles_fixture.label == 'style'
 
-    def test_find(self, item_images_fixture, settings):
+    def test_find(self, styles_fixture, settings):
         """It returns the absolute path to the fixture's file using the app label and fixture label."""
         settings.CHITON_ROOT = '/tmp'
 
-        path = item_images_fixture.find()
-        assert path == '/tmp/rack/fixtures/item_image.json'
+        path = styles_fixture.find()
+        assert path == '/tmp/runway/fixtures/style.json'
 
-    def test_is_needed(self, item_images_fixture):
+    def test_is_needed(self, styles_fixture):
         """It reports fixtures as needed by defaut."""
-        assert item_images_fixture.is_needed()
+        assert styles_fixture.is_needed()
 
-    def test_is_needed_initial(self, item_image_factory):
+    def test_is_needed_initial(self, style_factory):
         """It reports an initial fixture as unneeded if it has models."""
-        fixture = Fixture(ItemImage, initial=True)
+        fixture = Fixture(Style, initial=True)
         assert fixture.is_needed()
 
-        item_image_factory()
+        style_factory()
         assert not fixture.is_needed()
 
-    def test_is_needed_initial_queryset(self, item_image_factory):
+    def test_is_needed_initial_queryset(self, style_factory):
         """It reports an initial fixture as needed if its queryset does not exist."""
-        fixture = Fixture(ItemImage, queryset=ItemImage.objects.filter(height=100), initial=True)
+        fixture = Fixture(Style, queryset=Style.objects.filter(name='Classy'), initial=True)
 
-        item_image_factory(height=50)
+        style_factory(name='Sassy')
         assert fixture.is_needed()
 
-        item_image_factory(height=100)
+        style_factory(name='Classy')
         assert not fixture.is_needed()
 
-    def test_export(self, item_images_fixture):
+    def test_export(self, styles_fixture):
         """It writes a queryset's models to a JSON file."""
         with tempfile.NamedTemporaryFile() as output:
-            item_images_fixture.find = mock.Mock(return_value=output.name)
+            styles_fixture.find = mock.Mock(return_value=output.name)
 
-            exported = item_images_fixture.export()
+            exported = styles_fixture.export()
             assert exported == output.name
 
             with open(exported) as fixture_file:
@@ -65,15 +65,14 @@ class TestFixture:
 
             assert len(data) == 1
 
-            image = data[0]
-            assert 'fields' in image
-            assert 'model' in image
-            assert 'pk' in image
+            style = data[0]
+            assert 'fields' in style
+            assert 'model' in style
 
     def test_export_fields(self):
         """It can accept a subset of fields to serialize."""
-        ItemImage.objects.create(url='http://example.com', height=50, width=50)
-        fixture = Fixture(ItemImage, fields=['url', 'height'])
+        Style.objects.create(name='Classy', slug='classy')
+        fixture = Fixture(Style, fields=['name'])
 
         with tempfile.NamedTemporaryFile() as output:
             fixture.find = mock.Mock(return_value=output.name)
@@ -84,14 +83,12 @@ class TestFixture:
             with open(exported) as fixture_file:
                 data = json.load(fixture_file)
 
-            fields = data[0]['fields']
-            assert 'height' in fields
-            assert 'url' in fields
-            assert 'width' not in fields
+            fields = list(data[0]['fields'].keys())
+            assert fields == ['name']
 
-    def test_export_invalid_target(self, item_images_fixture):
+    def test_export_invalid_target(self, styles_fixture):
         """It raises an error when the target directory does not exist."""
-        item_images_fixture.find = mock.Mock(return_value='/abcdefghijklmnop/qrstuvwxyz.json')
+        styles_fixture.find = mock.Mock(return_value='/abcdefghijklmnop/qrstuvwxyz.json')
 
         with pytest.raises(FilesystemError):
-            item_images_fixture.export()
+            styles_fixture.export()
