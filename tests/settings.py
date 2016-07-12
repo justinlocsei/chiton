fa9adbf72503b283f2c8e03b27ca01c2cf87c0f1
Justin Locsei <justin.locsei@gmail.com>
2016-07-12 19:13:04 -0400

    Use a temporary directory as the test media root

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,15 @@
 import logging
+import os
+import shutil
+import tempfile
 
 from chiton.settings import *
+
+# Use a temporary directory for media files
+MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'chiton-test')
+if os.path.isdir(MEDIA_ROOT):
+    shutil.rmtree(MEDIA_ROOT)
+os.mkdir(MEDIA_ROOT)
 
 # Use a test-specific local-memory cache
 CACHES['default'] = {
