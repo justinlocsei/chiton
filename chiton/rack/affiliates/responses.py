a64a34e774696ea1e9c437b0ab59d48794e86291
Justin Locsei <justin.locsei@gmail.com>
2016-07-11 20:18:34 -0400

    Ensure that an affiliate item’s list of images is iterable

--- chiton/rack/affiliates/responses.py
+++ chiton/rack/affiliates/responses.py
@@ -46,7 +46,7 @@ ItemOverview = define_data_shape({
 # Details of an affiliate item returned by its API
 ItemDetails = define_data_shape({
     V.Required('availability'): V.Any([ItemAvailability], bool),
-    V.Required('images'): V.Any(None, [ItemImage]),
+    V.Required('images'): [ItemImage],
     V.Required('name'): V.All(str, V.Length(min=1)),
     V.Required('price'): Decimal
 })
