e82198e1dd7566d3179d5e286d730e184ec62d7a
Justin Locsei <justin.locsei@gmail.com>
2016-08-08 19:15:49 -0400

    Run the New Relic agent

--- chiton/wsgi.py
+++ chiton/wsgi.py
@@ -1,9 +1,12 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+import newrelic.agent
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chiton.settings')
 
-application = Sentry(get_wsgi_application())
+newrelic.agent.initialize(os.environ['NEW_RELIC_CONFIG_FILE'])
+
+application = Sentry(newrelic.agent.WSGIApplicationWrapper(get_wsgi_application()))
