41b6d3b050f61a6fd0553088ab4e5a10b62b2050
Justin Locsei <justin.locsei@gmail.com>
2016-08-17 21:49:13 -0400

    Bind cached-query signal handlers when the app is ready

--- chiton/core/apps.py
+++ chiton/core/apps.py
@@ -7,3 +7,8 @@ class Config(AppConfig):
     name = 'chiton.core'
     label = 'chiton'
     verbose_name = _('Core')
+
+    def ready(self):
+        """Bind signal handlers for cached queries when the app is ready."""
+        from chiton.core.queries import bind_signal_handlers
+        bind_signal_handlers()
