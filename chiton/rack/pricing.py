42b81b5d370bfb9895e970f12e388325e30da0b2
Justin Locsei <justin.locsei@gmail.com>
2016-08-22 11:19:04 -0400

    Generalize the wording of the logic for price-group cutoffs

--- chiton/rack/pricing.py
+++ chiton/rack/pricing.py
@@ -1,11 +1,15 @@
 from chiton.rack.models import AffiliateItem
 
 
+# The percentage for the upper and lower price groups
+EXTREME_CUTOFF = 0.25
+
+
 def update_basic_price_points(basic):
     """Update the price points for a basic using the price of its offerings.
 
-    This sets the budget and luxury points by examining the top and bottom
-    quarter of all items.
+    This sets the budget and luxury points by examining the lower and upper
+    extremes of the ordered garment prices.
 
     Args:
         basic (chiton.runway.models.Basic): A basic
@@ -22,9 +26,9 @@ def update_basic_price_points(basic):
     if not price_count:
         return (None, None)
 
-    quarter_index = max(int(price_count / 4) - 1, 0)
-    budget_end = prices[max(quarter_index, 0)]
-    luxury_start = prices[max(price_count - 1 - quarter_index, 0)]
+    cutoff_index = max(int(price_count * EXTREME_CUTOFF) - 1, 0)
+    budget_end = prices[max(cutoff_index, 0)]
+    luxury_start = prices[max(price_count - 1 - cutoff_index, 0)]
 
     basic.budget_end = budget_end
     basic.luxury_start = luxury_start
