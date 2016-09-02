ee9bc964f77e8661981a36529dca8136bcc30484
Justin Locsei <justin.locsei@gmail.com>
2016-09-02 09:39:37 -0400

    Add functions for encrypting and decrypting data

---
+++ chiton/core/encryption.py
@@ -0,0 +1,100 @@
+from base64 import b64decode, b64encode
+import binascii
+from django.conf import settings
+from nacl.exceptions import CryptoError
+from nacl.secret import SecretBox
+import nacl.utils
+
+
+MESSAGE_ENCODING = 'utf-8'
+OUTPUT_ENCODING = 'ascii'
+
+
+class EncryptionError(Exception):
+    """An error indicating issues during encryption or decryption."""
+
+
+def encrypt(message, key=None):
+    """Encrypt a message.
+
+    Args:
+        message (str): A message to encrypt
+
+    Keyword Args:
+        key (bytes): The secret key to use
+
+    Returns:
+        str: The encrypted message encoded in base64
+
+    Raises:
+        chiton.core.encryption.EncryptionError: If the key is invalid
+    """
+    box = _create_secret_box(key)
+    nonce = nacl.utils.random(SecretBox.NONCE_SIZE)
+
+    message_bytes = message.encode(MESSAGE_ENCODING)
+    encrypted_bytes = box.encrypt(message_bytes, nonce)
+
+    return b64encode(encrypted_bytes).decode(OUTPUT_ENCODING)
+
+
+def decrypt(value, key=None):
+    """Decrypt a message.
+
+    Args:
+        message (str): A message to decrypt
+
+    Keyword Args:
+        key (bytes): The secret key to use
+
+    Returns:
+        str: The decrypted message
+
+    Raises:
+        chiton.core.encryption.EncryptionError: If the key is invalid
+    """
+    box = _create_secret_box(key)
+
+    try:
+        encrypted_bytes = b64decode(value)
+    except binascii.Error as error:
+        raise EncryptionError(str(error))
+
+    try:
+        decrypted_bytes = box.decrypt(encrypted_bytes)
+    except CryptoError as error:
+        raise EncryptionError(str(error))
+
+    return decrypted_bytes.decode(MESSAGE_ENCODING)
+
+
+def rekey(encrypted, old_key=None, new_key=None):
+    """Re-encrypted an encrypted message using a new key.
+
+    Args:
+        encrypted (str): An encrypted message to re-encrypt
+
+    Keyword Args:
+        old_key (bytes): The key used to encrypt the message
+        new_key (bytes): The new key used to encrypt the message
+
+    Raises:
+        chiton.core.encryption.EncryptionError: If the re-encryption fails
+    """
+    decrypted_string = decrypt(encrypted, key=old_key)
+    return encrypt(decrypted_string, key=new_key)
+
+
+def _create_secret_box(key=None):
+    """Create a secret box with a given key.
+
+    Args:
+        key [bytes]: A secret key
+
+    Returns:
+        nacl.secret.SecretBox: A secret box using the given key
+    """
+    try:
+        return SecretBox(key or settings.CHITON_ENCRYPTION_KEY)
+    except (TypeError, ValueError) as error:
+        raise EncryptionError(str(error))
---
+++ tests/unit/core/test_encryption.py
@@ -0,0 +1,123 @@
+import pytest
+
+from chiton.core.encryption import decrypt, encrypt, rekey, EncryptionError
+
+
+class TestDecrypt():
+
+    def test_decrypt_encrypted(self):
+        """It decrypts encrypted messages."""
+        encrypted = encrypt('message')
+        decrypted = decrypt(encrypted)
+
+        assert decrypted == 'message'
+
+    def test_decrypt_encoding(self):
+        """It decrypts encoded messages as UTF-8 strings."""
+        encrypted = encrypt('méssåge')
+        decrypted = decrypt(encrypted)
+
+        assert decrypted == 'méssåge'
+
+    def test_decrypt_format(self):
+        """It raises an error when trying to decrypt a non-encrypted value."""
+        with pytest.raises(EncryptionError):
+            decrypt('message')
+
+    def test_decrypt_key(self):
+        """It accepts a custom decryption key."""
+        key = b'0' * 32
+
+        encrypted = encrypt('message', key=key)
+        assert decrypt(encrypted, key=key) == 'message'
+
+    def test_decrypt_key_invalid(self):
+        """It requires a 32-byte key."""
+        encrypted = encrypt('message', key=b'0' * 32)
+
+        with pytest.raises(EncryptionError):
+            decrypt(encrypted, key=b'0' * 31)
+
+    def test_decrypt_key_incorrect(self):
+        """It raises an error when an incorrect key is provided."""
+        right_key = b'0' * 32
+        wrong_key = b'1' * 32
+
+        encrypted = encrypt('message', key=right_key)
+
+        with pytest.raises(EncryptionError):
+            decrypt(encrypted, key=wrong_key)
+
+    def test_decrypt_key_default(self, settings):
+        """It gets its default key from settings."""
+        settings.CHITON_ENCRYPTION_KEY = b'0' * 32
+
+        encrypted = encrypt('message')
+        assert decrypt(encrypted) == 'message'
+
+        settings.CHITON_ENCRYPTION_KEY = b'1' * 32
+        with pytest.raises(EncryptionError):
+            decrypt(encrypted)
+
+
+class TestEncrypt():
+
+    def test_encrypt_encoding(self):
+        """It encrypts messages as base64-encoded strings."""
+        encrypted = encrypt('message')
+
+        assert encrypted
+        assert encrypted != 'message'
+        assert type(encrypted) == str
+
+    def test_encrypt_key(self):
+        """It accepts a custom encryption key."""
+        encrypted = encrypt('message', key=b'0' * 32)
+
+        assert encrypted
+        assert encrypted != 'message'
+
+    def test_encrypt_key_invalid(self):
+        """It requires a 32-byte key."""
+        with pytest.raises(EncryptionError):
+            encrypt('message', key=b'0' * 31)
+
+    def test_encrypt_key_default(self, settings):
+        """It gets its default key from settings."""
+        settings.CHITON_ENCRYPTION_KEY = None
+
+        with pytest.raises(EncryptionError):
+            encrypt('message')
+
+    def test_encrypt_nonce(self):
+        """It does not produce the same message using the same key."""
+        key = b'0' * 32
+        message = 'message'
+
+        assert encrypt(message, key=key) != encrypt(message, key=key)
+
+
+class TestRekey():
+
+    def test_rekey(self):
+        """It re-encrypts an encrypted message using a new key."""
+        old_key = b'0' * 32
+        new_key = b'1' * 32
+
+        old_encrypted = encrypt('message', key=old_key)
+        new_encrypted = rekey(old_encrypted, old_key=old_key, new_key=new_key)
+
+        assert decrypt(new_encrypted, key=new_key) == 'message'
+
+    def test_rekey_non_encrypted(self):
+        """It raises an error when trying to re-key a non-encrypted value."""
+        with pytest.raises(EncryptionError):
+            rekey('message', old_key=b'0' * 32, new_key=b'1' * 32)
+
+    def test_rekey_key_format(self):
+        """It raises an error when given an invalid new key."""
+        old_key = b'0' * 32
+        encrypted = encrypt('message', key=old_key)
+
+        with pytest.raises(EncryptionError):
+            rekey(encrypted, old_key=old_key, new_key=b'1' * 31)
